b858cd38f0b04a8c9d291cde2e5f8555fda1a05b
Ran Bi <biran@mit.edu>
2017-03-28 19:51:37 -0400

    add condor stuff

---
+++ condor/makeresiduals.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+
+tar -czvf residuals.tgz ../Corrections PbPb_Data_HiForest.list
---
+++ condor/skim-djet.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -lt 4 ]]; then
+    echo "usage: ./skim-djet.sh [n] [input list] [output dir] [residuals]"
+    exit 1
+fi
+
+tar -xzvf $4
+
+FILE=$(head -n$(($1+1)) $2 | tail -n1)
+
+echo ./D_jet_skim.exe $FILE ${1}.root akPu3PFJetAnalyzer 0 0 
+./D_jet_skim.exe $FILE ${1}.root akPu3PFJetAnalyzer 0 0
+
+mv ${1}.root ${3}
