4a082b5c7560782111c6bd2d45f8649389710cfa
Ran Bi <biran@mit.edu>
2017-03-29 18:22:05 -0400

    failed jobs' output are not moved to output dir

--- condor/skim-djet.sh
+++ condor/skim-djet.sh
@@ -9,7 +9,9 @@ tar -xzvf $4
 
 FILE=$(head -n$(($1+1)) $2 | tail -n1)
 
-echo ./D_jet_skim.exe $FILE ${1}.root akPu3PFJetAnalyzer 0 0 
+echo ./D_jet_skim.exe $FILE ${1}.root akPu3PFJetAnalyzer 0 0
 ./D_jet_skim.exe $FILE ${1}.root akPu3PFJetAnalyzer 0 0
 
-mv ${1}.root ${3}
+if [[ $? -eq 0 ]]; then
+    mv ${1}.root ${3}
+fi
