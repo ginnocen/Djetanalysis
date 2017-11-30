b0f8750c1e037bb6184c6b1ccbe0cb55f8e51ba6
Jing <boundino@gmail.com>
2017-11-29 22:11:12 -0500

    jetresolution: include centrality

--- jetresolution/jetreso_usehist.C
+++ jetresolution/jetreso_usehist.C
@@ -5,7 +5,9 @@ void jetreso_usehist(TString inputhistname, TString outputname,
 {
   int arguerr(TString collisionsyst);
   if(arguerr(collisionsyst)) return;
-
+  Int_t ispp = collisionsyst=="pp"?1:0;
+  setnCentBins(ispp);
+  
   xjjroot::setgstyle();
 
   if(createhists("usehist")) return;
@@ -13,185 +15,81 @@ void jetreso_usehist(TString inputhistname, TString outputname,
   if(!infhist->IsOpen()) return;
   if(gethists(infhist, "usehist")) return;
 
-  for(int j=0;j<nJtetaBins+1;j++)
+  std::vector<TString>  name          =  {"Pt",                        "PtCorr",                         "Phi",                     "Eta"};
+  std::vector<TH1F**>   ahHistoResoX  =  {(TH1F**)ahHistoResoPt,       (TH1F**)ahHistoResoPtCorr,        (TH1F**)ahHistoResoPhi,    (TH1F**)ahHistoResoEta};
+  std::vector<TH1F**>   hScaleX       =  {(TH1F**)hScalePt,            (TH1F**)hScalePtCorr,             (TH1F**)hScalePhi,         (TH1F**)hScaleEta};
+  std::vector<TH1F**>   hResoX        =  {(TH1F**)hResoPt,             (TH1F**)hResoPtCorr,              (TH1F**)hResoPhi,          (TH1F**)hResoEta};
+  std::vector<TString>  xtitle        =  {"p_{T}^{reco}/p_{T}^{gen}",  "p_{T}^{reco}/p_{T}^{gen},Corr",  "#phi^{reco}-#phi^{gen}",  "#eta^{reco}-#eta^{gen}"};
+  std::vector<Float_t>  xmin          =  {0,                           0,                                -0.08,                     -0.08};
+  std::vector<Float_t>  xmax          =  {2,                           2,                                0.08,                      0.08};
+  std::vector<Float_t>  ymin          =  {0,                           0,                                0,                         0};
+  std::vector<Float_t>  ymax          =  {0.2,                         0.2,                              (float)(ispp?0.3:0.1),     (float)(ispp?0.3:0.1)};
+  std::vector<Float_t>  param0        =  {1,                           1,                                0.01,                      0.01};
+  std::vector<Float_t>  param1        =  {1,                           1,                                0,                         0};
+  std::vector<Float_t>  param2        =  {0.2,                         0.2,                              0.01,                      0.01};
+
+  for(int k=0;k<nCentBins;k++)
     {
-      TString texjteta = j==0?"Incl. #eta":Form("%.1f < |#eta^{gen jet}| < %.1f", jtetaBins[j-1], jtetaBins[j]);
-      for(int i=0;i<nJtptBins;i++)
+      TString texcent = Form("Cent. %.0f - %.0f%s", centBins[k], centBins[k+1], "%");
+      for(int j=0;j<nJtetaBins+1;j++)
         {
-          TString texjtpt = Form("%.0f < p_{T}^{gen jet} < %.0f GeV", jtptBins[i], jtptBins[i+1]);
+          TString texjteta = j==0?"Incl. #eta":Form("%.1f < |#eta^{gen jet}| < %.1f", jtetaBins[j-1], jtetaBins[j]);
+          for(int i=0;i<nJtptBins;i++)
+            {
+              TString texjtpt = Form("%.0f < p_{T}^{gen jet} < %.0f GeV", jtptBins[i], jtptBins[i+1]);
 
-          TF1* fPt = new TF1("fPt", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", 0, 2);
-          fPt->SetLineWidth(3);
-          fPt->SetLineColor(kBlue);
-          fPt->SetParLimits(2, 0, 1);
-          fPt->SetParameter(0, 1);
-          fPt->SetParameter(1, 1);
-          fPt->SetParameter(2, 0.2);
-          TCanvas* cPt = new TCanvas("cPt", "", 600, 600);
-          TH2F* hemptyPt = new TH2F("hemptyPt", ";p_{T}^{reco} / p_{T}^{gen};Probability", 10, 0, 2, 10, 0, 0.2);
-          xjjroot::sethempty(hemptyPt, 0, 0.4);
-          hemptyPt->Draw();
-          ahHistoResoPt[j][i]->Scale(1./ahHistoResoPt[j][i]->Integral());
-          xjjroot::setthgrstyle(ahHistoResoPt[j][i], kBlack, 24, 1.2, kBlack, 1, 1, -1, -1, -1);
-          ahHistoResoPt[j][i]->Draw("same pe");
-          ahHistoResoPt[j][i]->Fit("fPt", "L q", "", 0, 2);
-          ahHistoResoPt[j][i]->Fit("fPt", "m q", "", 0, 2);
-          Float_t sigmaPt = fPt->GetParameter(2);
-          TF1* fPt1 = new TF1("fPt1", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", 0, 2);
-          fPt1->SetParameter(0, fPt->GetParameter(0));
-          fPt1->SetParameter(1, fPt->GetParameter(1));
-          fPt1->SetParameter(2, fPt->GetParameter(2));
-          fPt1->SetLineWidth(3);
-          fPt1->SetLineColor(kRed);
-          fPt1->Draw("same");
-          ahHistoResoPt[j][i]->Fit("fPt", "L q", "", 1-sigmaPt, 1+sigmaPt);
-          ahHistoResoPt[j][i]->Fit("fPt", "m q", "", 1-sigmaPt, 1+sigmaPt);
-          fPt->Draw("same");
-          hScalePt[j]->SetBinContent(i+1, fPt->GetParameter(1));
-          hScalePt[j]->SetBinError(i+1, fPt->GetParError(1));
-          hResoPt[j]->SetBinContent(i+1, fPt->GetParameter(2));
-          hResoPt[j]->SetBinError(i+1, fPt->GetParError(2));
-          Float_t texxpos = 0.22, texypos = 0.85, texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjtpt);
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjteta);
-          texxpos = 0.65; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#mu = %.3f #pm %.3f", fPt->GetParameter(1), fPt->GetParError(1)));
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#sigma = %.3f #pm %.3f", fPt->GetParameter(2), fPt->GetParError(2)));
-          xjjroot::drawCMS(collisionsyst);
-          cPt->SaveAs(Form("plotpulls/cfitPt_%s_%d_%d.pdf",outputname.Data(),j,i));
+              for(int l=0;l<4;l++)
+                {
+                  TF1* fX = new TF1("fX", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", xmin.at(l), xmax.at(l));
+                  fX->SetLineWidth(3);
+                  fX->SetLineColor(kBlue);
+                  fX->SetParLimits(2, 0, 1);
+                  fX->SetParameter(0, param0.at(l));
+                  fX->SetParameter(1, param1.at(l));
+                  fX->SetParameter(2, param2.at(l));
+                  TCanvas* cX = new TCanvas("cX", "", 600, 600);
+                  TH2F* hemptyX = new TH2F("hemptyX", Form(";%s;Probability", xtitle.at(l).Data()), 10, xmin.at(l), xmax.at(l), 10, ymin.at(l), ymax.at(l));
+                  xjjroot::sethempty(hemptyX, 0, 0.4);
+                  hemptyX->Draw();
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Scale(1./(ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Integral());
+                  xjjroot::setthgrstyle((ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i], kBlack, 24, 1.2, kBlack, 1, 1, -1, -1, -1);
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Draw("same pe");
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Fit("fX", "L q", "", xmin.at(l), xmax.at(l));
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Fit("fX", "m q", "", xmin.at(l), xmax.at(l));
+                  Float_t sigmaX = fX->GetParameter(2);
+                  TF1* fX1 = new TF1("fX1", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", xmin.at(l), xmax.at(l));
+                  fX1->SetParameter(0, fX->GetParameter(0));
+                  fX1->SetParameter(1, fX->GetParameter(1));
+                  fX1->SetParameter(2, fX->GetParameter(2));
+                  fX1->SetLineWidth(3);
+                  fX1->SetLineColor(kRed);
+                  fX1->Draw("same");
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Fit("fX", "L q", "", param1.at(l)-sigmaX, param1.at(l)+sigmaX);
+                  (ahHistoResoX.at(l))[k*nJtptBins*(nJtetaBins+1)+j*nJtptBins+i]->Fit("fX", "m q", "", param1.at(l)-sigmaX, param1.at(l)+sigmaX);
+                  fX->Draw("same");
+                  (hScaleX.at(l))[k*(nJtetaBins+1)+j]->SetBinContent(i+1, fX->GetParameter(1));
+                  (hScaleX.at(l))[k*(nJtetaBins+1)+j]->SetBinError(i+1, fX->GetParError(1));
+                  (hResoX.at(l))[k*(nJtetaBins+1)+j]->SetBinContent(i+1, fX->GetParameter(2));
+                  (hResoX.at(l))[k*(nJtetaBins+1)+j]->SetBinError(i+1, fX->GetParError(2));
+                  Float_t texxpos = 0.22, texypos = 0.85, texdypos = 0.06; texypos += texdypos;
+                  xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjtpt);
+                  xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjteta);
+                  if(!ispp) xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texcent);
+                  texxpos = 0.65; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
+                  xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#mu = %.3f #pm %.3f", fX->GetParameter(1), fX->GetParError(1)));
+                  xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#sigma = %.3f #pm %.3f", fX->GetParameter(2), fX->GetParError(2)));
+                  xjjroot::drawCMS(collisionsyst);
+                  cX->SaveAs(Form("plotpulls/cfit%s_%s_%d_%d_%d.pdf",name.at(l).Data(),outputname.Data(),k,j,i));
 
-          TF1* fPtCorr = new TF1("fPtCorr", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", 0, 2);
-          fPtCorr->SetLineWidth(3);
-          fPtCorr->SetLineColor(kBlue);
-          fPtCorr->SetParLimits(2, 0, 1);
-          fPtCorr->SetParameter(0, 1);
-          fPtCorr->SetParameter(1, 1);
-          fPtCorr->SetParameter(2, 0.2);
-          TCanvas* cPtCorr = new TCanvas("cPtCorr", "", 600, 600);
-          TH2F* hemptyPtCorr = new TH2F("hemptyPtCorr", ";p_{T}^{reco} / p_{T}^{gen};Probability", 10, 0, 2, 10, 0, 0.2);
-          xjjroot::sethempty(hemptyPtCorr, 0, 0.4);
-          hemptyPtCorr->Draw();
-          ahHistoResoPtCorr[j][i]->Scale(1./ahHistoResoPtCorr[j][i]->Integral());
-          xjjroot::setthgrstyle(ahHistoResoPtCorr[j][i], kBlack, 24, 1.2, kBlack, 1, 1, -1, -1, -1);
-          ahHistoResoPtCorr[j][i]->Draw("same pe");
-          ahHistoResoPtCorr[j][i]->Fit("fPtCorr", "L q", "", 0, 2);
-          ahHistoResoPtCorr[j][i]->Fit("fPtCorr", "m q", "", 0, 2);
-          Float_t sigmaPtCorr = fPtCorr->GetParameter(2);
-          TF1* fPtCorr1 = new TF1("fPtCorr1", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", 0, 2);
-          fPtCorr1->SetParameter(0, fPtCorr->GetParameter(0));
-          fPtCorr1->SetParameter(1, fPtCorr->GetParameter(1));
-          fPtCorr1->SetParameter(2, fPtCorr->GetParameter(2));
-          fPtCorr1->SetLineWidth(3);
-          fPtCorr1->SetLineColor(kRed);
-          fPtCorr1->Draw("same");
-          ahHistoResoPtCorr[j][i]->Fit("fPtCorr", "L q", "", 1-sigmaPtCorr, 1+sigmaPtCorr);
-          ahHistoResoPtCorr[j][i]->Fit("fPtCorr", "m q", "", 1-sigmaPtCorr, 1+sigmaPtCorr);
-          fPtCorr->Draw("same");
-          hScalePtCorr[j]->SetBinContent(i+1, fPtCorr->GetParameter(1));
-          hScalePtCorr[j]->SetBinError(i+1, fPtCorr->GetParError(1));
-          texxpos = 0.22; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjtpt);
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjteta);
-          texxpos = 0.65; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#mu = %.3f #pm %.3f", fPtCorr->GetParameter(1), fPtCorr->GetParError(1)));
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#sigma = %.3f #pm %.3f", fPtCorr->GetParameter(2), fPtCorr->GetParError(2)));
-          xjjroot::drawCMS(collisionsyst);
-          cPtCorr->SaveAs(Form("plotpulls/cfitPtCorr_%s_%d_%d.pdf",outputname.Data(),j,i));
-
-          TF1* fPhi = new TF1("fPhi", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", -0.08, 0.08);
-          fPhi->SetLineWidth(3);
-          fPhi->SetLineColor(kBlue);
-          fPhi->SetParameter(0, 0.01);
-          fPhi->SetParameter(1, 0);
-          fPhi->SetParameter(2, 0.01);
-          TCanvas* cPhi = new TCanvas("cPhi", "", 600, 600);
-          TH2F* hemptyPhi = new TH2F("hemptyPhi", ";#phi^{reco} - #phi^{gen};Probability", 10, -0.08, 0.08, 10, 0, 0.3);
-          xjjroot::sethempty(hemptyPhi, 0, 0.4);
-          hemptyPhi->GetXaxis()->SetNdivisions(505);
-          hemptyPhi->Draw();
-          ahHistoResoPhi[j][i]->Scale(1./ahHistoResoPhi[j][i]->Integral());
-          xjjroot::setthgrstyle(ahHistoResoPhi[j][i], kBlack, 24, 1.2, kBlack, 1, 1, -1, -1, -1);
-          ahHistoResoPhi[j][i]->Draw("same pe");
-          ahHistoResoPhi[j][i]->Fit("fPhi", "L q", "", -0.08, 0.08);
-          ahHistoResoPhi[j][i]->Fit("fPhi", "m q", "", -0.08, 0.08);
-          Float_t sigmaPhi = fPhi->GetParameter(2);
-          TF1* fPhi1 = new TF1("fPhi1", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", -0.08, 0.08);
-          fPhi1->SetParameter(0, fPhi->GetParameter(0));
-          fPhi1->SetParameter(1, fPhi->GetParameter(1));
-          fPhi1->SetParameter(2, fPhi->GetParameter(2));
-          fPhi1->SetLineWidth(3);
-          fPhi1->SetLineColor(kRed);
-          fPhi1->Draw("same");
-          ahHistoResoPhi[j][i]->Fit("fPhi", "L q", "", 0-sigmaPhi, 0+sigmaPhi);
-          ahHistoResoPhi[j][i]->Fit("fPhi", "m q", "", 0-sigmaPhi, 0+sigmaPhi);
-          fPhi->Draw("same");
-          hResoPhi[j]->SetBinContent(i+1, fPhi->GetParameter(2));
-          hResoPhi[j]->SetBinError(i+1, fPhi->GetParError(2));
-          texxpos = 0.22; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjtpt);
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjteta);
-          texxpos = 0.65; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#mu = %.3f #pm %.3f", fPhi->GetParameter(1), fPhi->GetParError(1)));
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#sigma = %.3f #pm %.3f", fPhi->GetParameter(2), fPhi->GetParError(2)));
-          xjjroot::drawCMS(collisionsyst);
-          cPhi->SaveAs(Form("plotpulls/cfitPhi_%s_%d_%d.pdf",outputname.Data(),j,i));
-
-          TF1* fEta = new TF1("fEta", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", -0.08, 0.08);
-          fEta->SetLineWidth(3);
-          fEta->SetLineColor(kBlue);
-          fEta->SetParameter(0, 0.01);
-          fEta->SetParameter(1, 0);
-          fEta->SetParameter(2, 0.01);
-          TCanvas* cEta = new TCanvas("cEta", "", 600, 600);
-          TH2F* hemptyEta = new TH2F("hemptyEta", ";#phi^{reco} - #phi^{gen};Probability", 10, -0.08, 0.08, 10, 0, 0.3);
-          xjjroot::sethempty(hemptyEta, 0, 0.4);
-          hemptyEta->GetXaxis()->SetNdivisions(505);
-          hemptyEta->Draw();
-          ahHistoResoEta[j][i]->Scale(1./ahHistoResoEta[j][i]->Integral());
-          xjjroot::setthgrstyle(ahHistoResoEta[j][i], kBlack, 24, 1.2, kBlack, 1, 1, -1, -1, -1);
-          ahHistoResoEta[j][i]->Draw("same pe");
-          ahHistoResoEta[j][i]->Fit("fEta", "L q", "", -0.08, 0.08);
-          ahHistoResoEta[j][i]->Fit("fEta", "m q", "", -0.08, 0.08);
-          Float_t sigmaEta = fEta->GetParameter(2);
-          TF1* fEta1 = new TF1("fEta1", "[0]*Gaus(x,[1],[2])/(TMath::Sqrt(2*3.14159265)*[2])", -0.08, 0.08);
-          fEta1->SetParameter(0, fEta->GetParameter(0));
-          fEta1->SetParameter(1, fEta->GetParameter(1));
-          fEta1->SetParameter(2, fEta->GetParameter(2));
-          fEta1->SetLineWidth(3);
-          fEta1->SetLineColor(kRed);
-          fEta1->Draw("same");
-          ahHistoResoEta[j][i]->Fit("fEta", "L q", "", 0-sigmaEta, 0+sigmaEta);
-          ahHistoResoEta[j][i]->Fit("fEta", "m q", "", 0-sigmaEta, 0+sigmaEta);
-          fEta->Draw("same");
-          hResoEta[j]->SetBinContent(i+1, fEta->GetParameter(2));
-          hResoEta[j]->SetBinError(i+1, fEta->GetParError(2));
-          texxpos = 0.22; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjtpt);
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), texjteta);
-          texxpos = 0.65; texypos = 0.85; texdypos = 0.06; texypos += texdypos;
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#mu = %.3f #pm %.3f", fEta->GetParameter(1), fEta->GetParError(1)));
-          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), Form("#sigma = %.3f #pm %.3f", fEta->GetParameter(2), fEta->GetParError(2)));
-          xjjroot::drawCMS(collisionsyst);
-          cEta->SaveAs(Form("plotpulls/cfitEta_%s_%d_%d.pdf",outputname.Data(),j,i));
-
-          delete hemptyEta;
-          delete cEta;
-          delete fEta;
-          delete fEta1;
-          delete hemptyPhi;
-          delete cPhi;
-          delete fPhi;
-          delete fPhi1;
-          delete hemptyPt;
-          delete cPt;
-          delete fPt;
-          delete fPt1;
-          delete hemptyPtCorr;
-          delete cPtCorr;
-          delete fPtCorr;
-          delete fPtCorr1;
+                  delete fX1;
+                  delete hemptyX;
+                  delete cX;
+                  delete fX;
+                }
+            }
         }
     }
+
   TFile* outf = new TFile(Form("rootfiles/reso_%s.root",outputname.Data()), "recreate");
   outf->cd();
   if(writehists("usehist")) return;
--- jetresolution/jetresolution.h
+++ jetresolution/jetresolution.h
@@ -3,6 +3,7 @@
 
 #include "../includes/djet.h"
 #include "../includes/prefilters.h"
+#include "../includes/paramCorr.h"
 #include "../includes/xjjcuti.h"
 #include "../includes/xjjrootuti.h"
 #include <iostream>
@@ -19,17 +20,24 @@ const int nJtptBins = sizeof(jtptBins)/sizeof(jtptBins[0])-1;
 Float_t jtetaBins[] = {0, 1.0, 1.6};
 const int nJtetaBins = sizeof(jtetaBins)/sizeof(jtetaBins[0])-1;
 Color_t jtetaColor[] = {kBlack, kBlue, kRed+2};
+Float_t centBins[] = {0, 30, 100};
+const int NCentBins = sizeof(centBins)/sizeof(centBins[0])-1;
+int nCentBins = 0;
+void setnCentBins(Int_t ispp) {nCentBins = ispp?1:NCentBins;}
 
-TH1F* ahHistoResoPt[nJtetaBins+1][nJtptBins];
-TH1F* ahHistoResoPtCorr[nJtetaBins+1][nJtptBins];
-TH1F* ahHistoResoPhi[nJtetaBins+1][nJtptBins];
-TH1F* ahHistoResoEta[nJtetaBins+1][nJtptBins];
+TH1F* ahHistoResoPt[NCentBins][nJtetaBins+1][nJtptBins];
+TH1F* ahHistoResoPtCorr[NCentBins][nJtetaBins+1][nJtptBins];
+TH1F* ahHistoResoPhi[NCentBins][nJtetaBins+1][nJtptBins];
+TH1F* ahHistoResoEta[NCentBins][nJtetaBins+1][nJtptBins];
 
-TH1F* hScalePt[nJtetaBins+1];
-TH1F* hScalePtCorr[nJtetaBins+1];
-TH1F* hResoPt[nJtetaBins+1];
-TH1F* hResoPhi[nJtetaBins+1];
-TH1F* hResoEta[nJtetaBins+1];
+TH1F* hScalePt[NCentBins][nJtetaBins+1];
+TH1F* hScalePtCorr[NCentBins][nJtetaBins+1];
+TH1F* hScalePhi[NCentBins][nJtetaBins+1];
+TH1F* hScaleEta[NCentBins][nJtetaBins+1];
+TH1F* hResoPt[NCentBins][nJtetaBins+1];
+TH1F* hResoPtCorr[NCentBins][nJtetaBins+1];
+TH1F* hResoPhi[NCentBins][nJtetaBins+1];
+TH1F* hResoEta[NCentBins][nJtetaBins+1];
 
 //
 int createhists(Option_t* option)
@@ -38,31 +46,40 @@ int createhists(Option_t* option)
   opt.ToLower();
   if(opt=="savehist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          for(int i=0;i<nJtptBins;i++)
+          for(int j=0;j<nJtetaBins+1;j++)
             {
-              ahHistoResoPt[j][i] = new TH1F(Form("hHistoResoPt_%d_%d",j,i), ";p_{T}^{reco} / p_{T}^{gen};", 50, 0, 2);
-              ahHistoResoPt[j][i]->Sumw2();
-              ahHistoResoPtCorr[j][i] = new TH1F(Form("hHistoResoPtCorr_%d_%d",j,i), ";p_{T}^{reco} / p_{T}^{gen};", 50, 0, 2);
-              ahHistoResoPtCorr[j][i]->Sumw2();
-              ahHistoResoPhi[j][i] = new TH1F(Form("hHistoResoPhi_%d_%d",j,i), ";#phi^{reco} - #phi^{gen};", 50, -0.08, 0.08);
-              ahHistoResoPhi[j][i]->Sumw2();
-              ahHistoResoEta[j][i] = new TH1F(Form("hHistoResoEta_%d_%d",j,i), ";#eta^{reco} - #eta^{gen};", 50, -0.08, 0.08);
-              ahHistoResoEta[j][i]->Sumw2();
+              for(int i=0;i<nJtptBins;i++)
+                {
+                  ahHistoResoPt[k][j][i] = new TH1F(Form("hHistoResoPt_%d_%d_%d",k,j,i), ";p_{T}^{reco} / p_{T}^{gen};", 50, 0, 2);
+                  ahHistoResoPt[k][j][i]->Sumw2();
+                  ahHistoResoPtCorr[k][j][i] = new TH1F(Form("hHistoResoPtCorr_%d_%d_%d",k,j,i), ";p_{T}^{reco} / p_{T}^{gen};", 50, 0, 2);
+                  ahHistoResoPtCorr[k][j][i]->Sumw2();
+                  ahHistoResoPhi[k][j][i] = new TH1F(Form("hHistoResoPhi_%d_%d_%d",k,j,i), ";#phi^{reco} - #phi^{gen};", 50, -0.08, 0.08);
+                  ahHistoResoPhi[k][j][i]->Sumw2();
+                  ahHistoResoEta[k][j][i] = new TH1F(Form("hHistoResoEta_%d_%d_%d",k,j,i), ";#eta^{reco} - #eta^{gen};", 50, -0.08, 0.08);
+                  ahHistoResoEta[k][j][i]->Sumw2();
+                }
             }
         }
       return 0;
     }
   if(opt=="usehist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          hResoPt[j] = new TH1F(Form("hResoPt_%d",j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
-          hResoPhi[j] = new TH1F(Form("hResoPhi_%d",j), ";Gen Jet p_{T} (GeV/c);#phi^{reco} - #phi^{gen}", nJtptBins, jtptBins);
-          hResoEta[j] = new TH1F(Form("hResoEta_%d",j), ";Gen Jet p_{T} (GeV/c);#eta^{reco} - #eta^{gen}", nJtptBins, jtptBins);
-          hScalePt[j] = new TH1F(Form("hScalePt_%d",j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
-          hScalePtCorr[j] = new TH1F(Form("hScalePtCorr_%d",j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+          for(int j=0;j<nJtetaBins+1;j++)
+            {
+              hResoPt[k][j] = new TH1F(Form("hResoPt_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+              hResoPtCorr[k][j] = new TH1F(Form("hResoPtCorr_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+              hResoPhi[k][j] = new TH1F(Form("hResoPhi_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);#phi^{reco} - #phi^{gen}", nJtptBins, jtptBins);
+              hResoEta[k][j] = new TH1F(Form("hResoEta_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);#eta^{reco} - #eta^{gen}", nJtptBins, jtptBins);
+              hScalePt[k][j] = new TH1F(Form("hScalePt_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+              hScalePtCorr[k][j] = new TH1F(Form("hScalePtCorr_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+              hScalePhi[k][j] = new TH1F(Form("hScalePhi_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+              hScaleEta[k][j] = new TH1F(Form("hScaleEta_%d_%d",k,j), ";Gen Jet p_{T} (GeV/c);p_{T}^{reco} / p_{T}^{gen}", nJtptBins, jtptBins);
+            }
         }
       return 0;
     }
@@ -76,27 +93,36 @@ int gethists(TFile* inf, Option_t* option)
   opt.ToLower();
   if(opt=="usehist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          for(int i=0;i<nJtptBins;i++)
+          for(int j=0;j<nJtetaBins+1;j++)
             {
-              ahHistoResoPt[j][i] = (TH1F*)inf->Get(Form("hHistoResoPt_%d_%d",j,i));
-              ahHistoResoPtCorr[j][i] = (TH1F*)inf->Get(Form("hHistoResoPtCorr_%d_%d",j,i));
-              ahHistoResoPhi[j][i] = (TH1F*)inf->Get(Form("hHistoResoPhi_%d_%d",j,i));
-              ahHistoResoEta[j][i] = (TH1F*)inf->Get(Form("hHistoResoEta_%d_%d",j,i));
+              for(int i=0;i<nJtptBins;i++)
+                {
+                  ahHistoResoPt[k][j][i] = (TH1F*)inf->Get(Form("hHistoResoPt_%d_%d_%d",k,j,i));
+                  ahHistoResoPtCorr[k][j][i] = (TH1F*)inf->Get(Form("hHistoResoPtCorr_%d_%d_%d",k,j,i));
+                  ahHistoResoPhi[k][j][i] = (TH1F*)inf->Get(Form("hHistoResoPhi_%d_%d_%d",k,j,i));
+                  ahHistoResoEta[k][j][i] = (TH1F*)inf->Get(Form("hHistoResoEta_%d_%d_%d",k,j,i));
+                }
             }
         }
       return 0;
     }
   if(opt=="plothist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          hResoPt[j] = (TH1F*)inf->Get(Form("hResoPt_%d",j));
-          hResoPhi[j] = (TH1F*)inf->Get(Form("hResoPhi_%d",j));
-          hResoEta[j] = (TH1F*)inf->Get(Form("hResoEta_%d",j));
-          hScalePt[j] = (TH1F*)inf->Get(Form("hScalePt_%d",j));
-          hScalePtCorr[j] = (TH1F*)inf->Get(Form("hScalePtCorr_%d",j));
+          for(int j=0;j<nJtetaBins+1;j++)
+            {
+              hResoPt[k][j] = (TH1F*)inf->Get(Form("hResoPt_%d_%d",k,j));
+              hResoPtCorr[k][j] = (TH1F*)inf->Get(Form("hResoPtCorr_%d_%d",k,j));
+              hResoPhi[k][j] = (TH1F*)inf->Get(Form("hResoPhi_%d_%d",k,j));
+              hResoEta[k][j] = (TH1F*)inf->Get(Form("hResoEta_%d_%d",k,j));
+              hScalePt[k][j] = (TH1F*)inf->Get(Form("hScalePt_%d_%d",k,j));
+              hScalePtCorr[k][j] = (TH1F*)inf->Get(Form("hScalePtCorr_%d_%d",k,j));
+              hScalePhi[k][j] = (TH1F*)inf->Get(Form("hScalePhi_%d_%d",k,j));
+              hScaleEta[k][j] = (TH1F*)inf->Get(Form("hScaleEta_%d_%d",k,j));
+            }
         }
       return 0;
     }
@@ -110,27 +136,36 @@ int writehists(Option_t* option)
   opt.ToLower();
   if(opt=="savehist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          for(int i=0;i<nJtptBins;i++)
+          for(int j=0;j<nJtetaBins+1;j++)
             {
-              ahHistoResoPt[j][i]->Write();
-              ahHistoResoPtCorr[j][i]->Write();
-              ahHistoResoPhi[j][i]->Write();
-              ahHistoResoEta[j][i]->Write();
+              for(int i=0;i<nJtptBins;i++)
+                {
+                  ahHistoResoPt[k][j][i]->Write();
+                  ahHistoResoPtCorr[k][j][i]->Write();
+                  ahHistoResoPhi[k][j][i]->Write();
+                  ahHistoResoEta[k][j][i]->Write();
+                }
             }
         }
       return 0;
     }
   if(opt=="usehist")
     {
-      for(int j=0;j<nJtetaBins+1;j++)
+      for(int k=0;k<nCentBins;k++)
         {
-          hResoPt[j]->Write();
-          hResoPhi[j]->Write();
-          hResoEta[j]->Write();
-          hScalePt[j]->Write();
-          hScalePtCorr[j]->Write();
+          for(int j=0;j<nJtetaBins+1;j++)
+            {
+              hResoPt[k][j]->Write();
+              hResoPtCorr[k][j]->Write();
+              hResoPhi[k][j]->Write();
+              hResoEta[k][j]->Write();
+              hScalePt[k][j]->Write();
+              hScalePtCorr[k][j]->Write();
+              hScalePhi[k][j]->Write();
+              hScaleEta[k][j]->Write();
+            }
         }
       return 0;
     }
