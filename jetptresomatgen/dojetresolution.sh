ae9541555f720424f4fbe8499341dc6e0071d2e2
Jing <boundino@gmail.com>
2018-02-06 23:15:53 -0500

    add jetptresomatgen

---
+++ jetptresomatgen/dojetresolution.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# dojetresolution.sh #
+
+DO_SAVEHIST=${1:-0}
+DO_USEHIST=${2:-0}
+DO_PLOTHIST=${3:-0}
+
+ifCorr=1
+# Select the systems the macros run on 
+iCOL=(0 1)
+
+##
+
+# nCOL loop
+COLSYST=('pp' 'PbPb')
+
+# dataset[nCOL]
+INPUTDANAME=(
+    '/export/d00/scratch/jwang/Djets/MC/DjetFiles_20171215_pp_5TeV_TuneCUETP8M1_Dfinder_MC_20171214_pthatweight.root'
+    '/export/d00/scratch/jwang/Djets/MC/DjetFiles_20171215_PbPb_5TeV_TuneCUETP8M1_Dfinder_MC_20171214_pthatweight.root'
+)
+
+# Do not touch the macros below if you don't know what they mean #
+
+[[ $DO_SAVEHIST -eq 0 && $DO_USEHIST -eq 0 && $DO_PLOTHIST -eq 0 ]] && echo "./dojetresolution.sh [DO_SAVEHIST] [DO_USEHIST] [DO_PLOTHIST]"
+
+#
+nCOL=${#COLSYST[@]}
+
+#
+NC='\033[0m'
+FUNCOLOR='\033[1;33m'
+ARGCOLOR='\033[1;32m'
+ERRCOLOR='\033[1;31m'
+
+#
+function float_to_string()
+{
+    if [[ $# -ne 1 ]]
+    then
+        echo -e "${ERRCOLOR}error:${NC} invalid argument number - float_to_string()"
+        return 1
+    fi
+    part1=`echo $1 | awk -F "." '{print $1}'`
+    part2=`echo $1 | awk -F "." '{print $2}'`
+    rt_float_to_string=${part1:-0}p${part2:-0}
+    echo $rt_float_to_string
+}
+
+function produce_postfix()
+{
+    if [[ $# -ne 1 ]]
+    then
+        echo -e "\033[1;31merror:${NC} invalid argument number - produce_postfix()"
+        return 1
+    fi
+    echo ${COLSYST[$1]}
+}
+
+#
+FOLDERS=("rootfiles" "plotresos" "plotfits" "plotfitsall")
+for i in ${FOLDERS[@]}
+do
+    if [[ ! -d $i ]]
+    then
+	mkdir -p $i
+    fi
+done
+
+##
+
+# jetreso_savehist.C #
+
+g++ jetreso_savehist.C $(root-config --cflags --libs) -g -o jetreso_savehist.exe || return 1;
+for i in ${iCOL[@]}
+do
+    tPOSTFIX=Djet_reso_$(produce_postfix $i)
+    if [[ $DO_SAVEHIST -eq 1 ]]
+    then
+        echo -e "-- Processing ${FUNCOLOR}jetreso_savehist.C${NC} :: ${ARGCOLOR}${COLSYST[i]}${NC}"
+        ./jetreso_savehist.exe "${INPUTDANAME[i]}" "rootfiles/hist_${tPOSTFIX}" "${COLSYST[i]}" "$ifCorr" &
+        echo
+    fi
+done
+wait
+rm jetreso_savehist.exe
+
+
+# jetreso_usehist.C #
+
+g++ jetreso_usehist.C $(root-config --cflags --libs) -g -o jetreso_usehist.exe || return 1;
+if [[ $DO_USEHIST -eq 1 ]]
+then
+    for i in ${iCOL[@]}
+    do
+        tPOSTFIX=Djet_reso_$(produce_postfix $i)
+        echo -e "-- Processing ${FUNCOLOR}jetreso_usehist.C${NC} :: ${ARGCOLOR}${COLSYST[i]}${NC}"
+        [[ ! -f "rootfiles/hist_${tPOSTFIX}.root" ]] && { echo -e "${ERRCOLOR}error:${NC} rootfiles/hist_${tPOSTFIX}.root doesn't exist. Process jetreso_savehist.C first."; continue; }
+        ./jetreso_usehist.exe "rootfiles/hist_${tPOSTFIX}" "${tPOSTFIX}" "${COLSYST[i]}"
+        echo
+    done
+fi
+rm jetreso_usehist.exe
+
+# jetreso_plothist.C #
+
+g++ jetreso_plothist.C $(root-config --cflags --libs) -g -o jetreso_plothist.exe || return 1;
+g++ jetreso_plotpar.C $(root-config --cflags --libs) -g -o jetreso_plotpar.exe || return 1;
+if [[ $DO_PLOTHIST -eq 1 ]]
+then
+    for i in ${iCOL[@]}
+    do
+        tPOSTFIX=Djet_reso_$(produce_postfix $i)
+        echo -e "-- Processing ${FUNCOLOR}jetreso_plothist.C${NC} :: ${ARGCOLOR}${COLSYST[i]}${NC}"
+        [[ ! -f "rootfiles/reso_${tPOSTFIX}.root" ]] && { echo -e "${ERRCOLOR}error:${NC} rootfiles/reso_${tPOSTFIX}.root doesn't exist. Process jetreso_usehist.C first."; continue; }
+        ./jetreso_plothist.exe "rootfiles/reso_${tPOSTFIX}" "$tPOSTFIX" "${COLSYST[i]}"
+        echo
+    done
+    ./jetreso_plotpar.exe
+fi
+rm jetreso_plotpar.exe
+rm jetreso_plothist.exe
