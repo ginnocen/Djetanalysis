5dd438947caa0d6cf803120d030aa8482df4cb98
Jing <boundino@gmail.com>
2018-02-06 23:08:17 -0500

    update headers

--- includes/prefilters.h
+++ includes/prefilters.h
@@ -6,11 +6,28 @@
 #include <vector>
 #include <map>
 #include <TString.h>
+#include "centweight.h"
+
+#ifndef _JTPTBINS
+#define _JTPTBINS
+Float_t jtptBins[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 1000};
+const int nJtptBins = sizeof(jtptBins)/sizeof(jtptBins[0])-1;
+#endif
+#ifndef _RESOJTPTBINS
+#define _RESOJTPTBINS
+Float_t resojtptBins[] = {10, 20, 30, 40, 50, 60, 70, 80, 100, 120, 140, 160, 180, 200, 250, 300};
+const int nResoJtptBins = sizeof(resojtptBins)/sizeof(resojtptBins[0])-1;
+#endif
+#ifndef _RESOJTDRBINS
+#define _RESOJTDRBINS
+Float_t resojtdrBins[] = {0, 0.01, 0.03, 0.05, 0.07, 0.1, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70};
+const int nResoJtdrBins = sizeof(resojtdrBins)/sizeof(resojtdrBins[0])-1;
+#endif
 
 Float_t ptBins[] = {4, 20, 999};
 // Float_t drBins[] = {0, 0.05, 0.1, 0.2, 0.3, 0.5};
-Float_t drBins[] = {0, 0.05, 0.1, 0.3, 0.5};
-Float_t zBins[] = {0, 0.2, 0.4, 0.6, 0.8, 1.0};
+Float_t drBins[] = {0, 0.05, 0.1, 0.30, 0.50};
+Float_t zBins[] = {0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.90, 1.0};
 const int nCoBins = 2;
 std::map<TString, int> collsyst_list = {{"pp", 0}, {"PbPb", 1}};
 TString tMC[] = {"data", "MC"};
@@ -31,54 +48,54 @@ std::vector<TString> cutval_list_skim[nCoBins] = {{"pBeamScrapingFilter", "pPApr
 Float_t cutval_Dy = 2.0;
 /*
   {
-    // ----- dR bin -----
-    {{xx, xx, xx, xx, xx},  // pp 4-20 GeV
-     {xx, xx, xx, xx, xx}}, // pp 20-999 GeV
-    {{xx, xx, xx, xx, xx},  // PbPb 4-20 GeV
-     {xx, xx, xx, xx, xx}}  // PbPb 20-999 GeV
+  // ----- dR bin -----
+  {{xx, xx, xx, xx, xx},  // pp 4-20 GeV
+  {xx, xx, xx, xx, xx}}, // pp 20-999 GeV
+  {{xx, xx, xx, xx, xx},  // PbPb 4-20 GeV
+  {xx, xx, xx, xx, xx}}  // PbPb 20-999 GeV
   };
 */
 Float_t cutval_list_Dsvpv[nCoBins][nPtBins][nDrBins] = 
   {
-    {{3.26,  3.26,  3.23,  3.57},
-     {2.86,  2.86,  2.34,  3.22}},
-    {{4.44,  4.44,  4.34,  4.34},
-     {2.68,  2.68,  2.08,  2.08}}
+    {{3.00, 3.00, 3.00, 3.00},
+     {3.00, 3.00, 3.00, 3.00}},
+    {{3.00, 3.00, 3.00, 3.00},
+     {3.00, 3.00, 3.00, 3.00}}
   };
 Float_t cutval_list_Dalpha[nCoBins][nPtBins][nDrBins] = 
   {
-    {{0.039,  0.039,  0.046,  0.046},
-     {0.037,  0.037,  0.039,  0.014}},
-    {{0.040,  0.040,  0.038,  0.049},
-     {0.038,  0.038,  0.107,  0.107}}
+    {{0.04, 0.04, 0.04, 0.04},
+     {0.04, 0.04, 0.04, 0.04}},
+    {{0.04, 0.04, 0.04, 0.04},
+     {0.04, 0.04, 0.04, 0.04}}
   };
 Float_t cutval_list_Dchi2cl[nCoBins][nPtBins][nDrBins] = 
   {
-    {{0.05,  0.05,  0.05,  0.05},
-     {0.05,  0.05,  0.05,  0.05}},
-    {{0.05,  0.05,  0.05,  0.05},
-     {0.05,  0.05,  0.05,  0.05}}
+    {{0.05, 0.05, 0.05, 0.05},
+     {0.05, 0.05, 0.05, 0.05}},
+    {{0.05, 0.05, 0.05, 0.05},
+     {0.05, 0.05, 0.05, 0.05}}
   };
 Float_t cutval_list_trkPt[nCoBins][nPtBins][nDrBins] = 
   {
-    {{2.0,  2.0,  2.0,  2.0},
-     {2.0,  2.0,  2.0,  2.0}},
-    {{2.0,  2.0,  2.0,  2.0},
-     {2.0,  2.0,  2.0,  2.0}}
+    {{2.00, 2.00, 2.00, 2.00},
+     {2.00, 2.00, 2.00, 2.00}},
+    {{2.00, 2.00, 2.00, 2.00},
+     {2.00, 2.00, 2.00, 2.00}}
   };
 Float_t cutval_list_trkEta[nCoBins][nPtBins][nDrBins] = 
   {
-    {{2.0,  2.0,  2.0,  2.0},
-     {2.0,  2.0,  2.0,  2.0}},
-    {{2.0,  2.0,  2.0,  2.0},
-     {2.0,  2.0,  2.0,  2.0}}
+    {{2.00, 2.00, 2.00, 2.00},
+     {2.00, 2.00, 2.00, 2.00}},
+    {{2.00, 2.00, 2.00, 2.00},
+     {2.00, 2.00, 2.00, 2.00}}
   };
 Float_t cutval_list_trkPtErr[nCoBins][nPtBins][nDrBins] = 
   {
-    {{0.3,  0.3,  0.3,  0.3},
-     {0.3,  0.3,  0.3,  0.3}},
-    {{0.3,  0.3,  0.3,  0.3},
-     {0.3,  0.3,  0.3,  0.3}}
+    {{0.30, 0.30, 0.30, 0.30},
+     {0.30, 0.30, 0.30, 0.30}},
+    {{0.30, 0.30, 0.30, 0.30},
+     {0.30, 0.30, 0.30, 0.30}}
   };
 
 //
