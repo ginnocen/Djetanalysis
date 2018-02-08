c49f05a7249af8521efe212cbf3776bd67291750
Jing <boundino@gmail.com>
2018-02-08 17:42:55 -0500

    xcheckscale

---
+++ xcheckscale/djtclosure.h
@@ -0,0 +1,129 @@
+#ifndef _DJTCLOSURE_H_
+#define _DJTCLOSURE_H_
+
+#include "../includes/prefilters.h"
+#include "../includes/xjjcuti.h"
+#include "../includes/xjjrootuti.h"
+#include <iostream>
+#include <iomanip>
+#include <TMath.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TLegend.h>
+#include <TCanvas.h>
+#include <TFile.h>
+
+const Float_t DZERO_MASS = 1.8649;
+
+//
+const int nRefBins = 2;
+TString tRef[nRefBins] = {"eta", "etaref"};
+const int nCases = 2;
+TString tCases[nCases] = {"frag_dep", "frag_indep"};
+TString legCases[nCases] = {"Frag-dep JEC", "Frag-indep JEC"};
+
+//
+Color_t amcolor[nCases] = {kRed+1, kBlack};
+Style_t amstyle[nRefBins] = {20, 24};
+
+// TH1F* hNjets;
+
+TH1F* ahSignalRnorm[nCases][nRefBins][nPtBins];
+TH1F* ahSignalZnorm[nCases][nRefBins][nPtBins];
+TH1F* ahSignalRnormPull[nCases][nRefBins][nPtBins];
+TH1F* ahSignalZnormPull[nCases][nRefBins][nPtBins];
+TH1F* ahSignalRsub[nCases][nPtBins];
+TH1F* ahSignalZsub[nCases][nPtBins];
+TH1F* ahSignalRsubPull[nCases][nPtBins];
+TH1F* ahSignalZsubPull[nCases][nPtBins];
+
+TH1F* ahSignalRsubRatio[nCases][nPtBins];
+TH1F* ahSignalZsubRatio[nCases][nPtBins];
+TH1F* ahSignalRsubRatioPull[nCases][nPtBins];
+TH1F* ahSignalZsubRatioPull[nCases][nPtBins];
+
+
+//
+int createhists(Option_t* option)
+{
+  TString opt  = option;
+  opt.ToLower();
+
+  if(opt=="plothist")
+    {
+      for(int m=0;m<nCases;m++)
+        {
+          for(int i=0;i<nPtBins;i++)
+            {
+              for(int l=0;l<nRefBins;l++)
+                {
+                  ahSignalRnormPull[m][l][i] = new TH1F(Form("hSignalRnormPull_%s_pt_%d_%s",tRef[l].Data(),i,tCases[m].Data()), ";;", nDrBins, drBins);
+                  ahSignalZnormPull[m][l][i] = new TH1F(Form("hSignalZnormPull_%s_pt_%d_%s",tRef[l].Data(),i,tCases[m].Data()), ";;", nZBins, zBins);
+                }
+              ahSignalRsubPull[m][i] = new TH1F(Form("hSignalRsubPull_pt_%d_%s",i,tCases[m].Data()), ";;", nDrBins, drBins);
+              ahSignalZsubPull[m][i] = new TH1F(Form("hSignalZsubPull_pt_%d_%s",i,tCases[m].Data()), ";;", nZBins, zBins);
+            }
+        }
+      return 0;
+    }
+  if(opt=="plotratio")
+    {
+      for(int m=0;m<nCases;m++)
+        {
+          for(int i=0;i<nPtBins;i++)
+            {
+              ahSignalRsubRatioPull[m][i] = new TH1F(Form("hSignalRsubRatioPull_pt_%d_%s",i,tCases[m].Data()), ";;", nDrBins, drBins);
+              ahSignalZsubRatioPull[m][i] = new TH1F(Form("hSignalZsubRatioPull_pt_%d_%s",i,tCases[m].Data()), ";;", nZBins, zBins);
+            }
+        }
+      return 0;
+    }
+  std::cout<<"error: invalid option for gethists()"<<std::endl;
+  return 1;
+}
+
+int gethists(std::vector<TFile*> inf, Option_t* option)
+{
+  TString opt  = option;
+  opt.ToLower();
+
+  if(opt=="plothist")
+    {
+      for(int m=0;m<nCases;m++)
+        {
+          for(int i=0;i<nPtBins;i++)
+            {
+              for(int l=0;l<nRefBins;l++)
+                {
+                  ahSignalRnorm[m][l][i] = (TH1F*)inf[m]->Get(Form("hSignalRnorm_%s_pt_%d",tRef[l].Data(),i));
+                  ahSignalRnorm[m][l][i]->SetName(Form("hSignalRnorm_%s_pt_%d_%s",tRef[l].Data(),i,tCases[m].Data()));
+                  ahSignalZnorm[m][l][i] = (TH1F*)inf[m]->Get(Form("hSignalZnorm_%s_pt_%d",tRef[l].Data(),i));
+                  ahSignalZnorm[m][l][i]->SetName(Form("hSignalZnorm_%s_pt_%d_%s",tRef[l].Data(),i,tCases[m].Data()));
+                }
+              ahSignalRsub[m][i] = (TH1F*)inf[m]->Get(Form("hSignalRsub_pt_%d",i));
+              ahSignalRsub[m][i]->SetName(Form("hSignalRsub_pt_%d_%s",i,tCases[m].Data()));
+              ahSignalZsub[m][i] = (TH1F*)inf[m]->Get(Form("hSignalZsub_pt_%d",i));
+              ahSignalZsub[m][i]->SetName(Form("hSignalZsub_pt_%d_%s",i,tCases[m].Data()));
+            }
+        }
+      return 0;
+    }
+  if(opt=="plotratio")
+    {
+      for(int m=0;m<nCases;m++)
+        {
+          for(int i=0;i<nPtBins;i++)
+            {
+              ahSignalRsubRatio[m][i] = (TH1F*)inf[m]->Get(Form("hSignalRsubRatio_pt_%d",i));
+              ahSignalRsubRatio[m][i]->SetName(Form("hSignalRsubRatio_pt_%d_%s",i,tCases[m].Data()));
+              ahSignalZsubRatio[m][i] = (TH1F*)inf[m]->Get(Form("hSignalZsubRatio_pt_%d",i));
+              ahSignalZsubRatio[m][i]->SetName(Form("hSignalZsubRatio_pt_%d_%s",i,tCases[m].Data()));
+            }
+        }
+      return 0;
+    }
+  std::cout<<"error: invalid option for gethists()"<<std::endl;
+  return 1;
+}
+
+#endif
---
+++ xcheckscale/djtclosure_plothist.C
@@ -0,0 +1,184 @@
+#include "djtclosure.h"
+
+void djtclosure_plothist(std::vector<TString> inputhistname, TString outputname,
+                         TString collisionsyst,
+                         Float_t jetptmin, Float_t jetetamin, Float_t jetetamax,
+                         Int_t plotref = 0)
+{
+  int arguerr(std::vector<TString>inputhistname, TString collisionsyst, Int_t plotref);
+  if(arguerr(inputhistname, collisionsyst, plotref)) return;
+
+  if(createhists("plothist")) return;
+
+  std::vector<TFile*> infhist(nCases, 0);
+  for(int m=0;m<nCases;m++)
+    {
+      infhist[m] = new TFile(Form("%s.root",inputhistname[m].Data()));
+      if(!infhist[m]->IsOpen()) return;
+    }
+  if(gethists(infhist, "plothist")) return;
+
+  // preparation
+  std::vector<TString> vectex =
+    {
+      "|y^{D}| < 2",
+      Form("p_{T}^{jet} > %s GeV/c",xjjc::number_remove_zero(jetptmin).c_str()),
+      Form("%s < |#eta^{jet}| < %s",xjjc::number_remove_zero(jetetamin).c_str(),xjjc::number_remove_zero(jetetamax).c_str()),
+    };
+  std::vector<float> vdrBins, vzBins;
+  for(int j=0;j<sizeof(drBins)/sizeof(drBins[0]);j++) vdrBins.push_back(drBins[j]);
+  for(int j=0;j<sizeof(zBins)/sizeof(zBins[0]);j++) vzBins.push_back(zBins[j]);
+  std::vector<TString>            xtitle           = {"#DeltaR",                 "z = p^{D}_{T} / p^{jet}_{T}"};
+  std::vector<TString>            ytitle           = {"#rho(#DeltaR)",           "#rho(z)"};
+  std::vector<TString>            tname            = {"dr",                      "z"};
+  std::vector<std::vector<float>> vxBins           = {vdrBins,                   vzBins};
+  std::vector<TH1F**>             hSignalXnorm     = {(TH1F**)ahSignalRnorm,     (TH1F**)ahSignalZnorm};
+  std::vector<TH1F**>             hSignalXnormPull = {(TH1F**)ahSignalRnormPull, (TH1F**)ahSignalZnormPull};
+  std::vector<TH1F**>             hSignalXsub      = {(TH1F**)ahSignalRsub,      (TH1F**)ahSignalZsub};
+  std::vector<TH1F**>             hSignalXsubPull  = {(TH1F**)ahSignalRsubPull,  (TH1F**)ahSignalZsubPull};
+  std::vector<float>              yPullaxismin     = {0.2,                       0.2};
+  std::vector<float>              yPullaxismax     = {2.2,                       2.2};
+
+  Float_t yaxismin = 1.1e-3, yaxismax = 1.e+3;
+  Float_t ypaddiv = 2./3, yPullpaddiv = 1-ypaddiv;
+
+  // calculate pull
+  for(int k=0;k<2;k++)
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int m=0;m<nCases;m++)
+            {
+              for(int l=0;l<nRefBins;l++)
+                {
+                  if(l && !plotref) continue;
+                  (hSignalXnormPull.at(k))[(m*nRefBins+l)*nPtBins+i]->Divide((hSignalXnorm.at(k))[(0*nRefBins+l)*nPtBins+i], 
+                                                                             (hSignalXnorm.at(k))[(1*nRefBins+l)*nPtBins+i]); // hSignalXnorm[nCases-1][l][i] is truth
+                }
+              (hSignalXsubPull.at(k))[m*nPtBins+i]->Divide((hSignalXsub.at(k))[0*nPtBins+i], 
+                                                           (hSignalXsub.at(k))[1*nPtBins+i]); // hSignalXsub[nCases-1][i] is truth
+            }
+        }
+    }
+
+  // plot
+  for(int k=0;k<2;k++)
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          TCanvas* c = new TCanvas("c", "", 600, 800);
+          TPad* pXsec = new TPad("pXsec", "", 0, 1-ypaddiv, 1, 1);
+          pXsec->SetMargin(xjjroot::margin_pad_left, xjjroot::margin_pad_right, 0, xjjroot::margin_pad_top);
+          pXsec->Draw();
+          pXsec->cd();
+          pXsec->SetLogy();
+          TH2F* hempty = new TH2F("hempty", Form(";%s;%s",xtitle[k].Data(),ytitle[k].Data()), 5, vxBins[k].front(), vxBins[k].back(), 10, yaxismin, yaxismax);
+          hempty->GetXaxis()->SetNdivisions(505);
+          xjjroot::sethempty(hempty, 0, 0);
+          hempty->Draw();
+          TLegend* leg = new TLegend(0.58, 0.88-nCases*0.06, 0.90, 0.88);
+          xjjroot::setleg(leg);
+          TLegend* legref = new TLegend(0.56, 0.17-xjjroot::margin_pad_bottom, 1.30, 0.23-xjjroot::margin_pad_bottom);
+          xjjroot::setleg(legref);
+          legref->SetNColumns(nCases);
+          for(int l=0;l<nRefBins;l++)
+            {
+              if(l && !plotref) continue;
+              for(int m=nCases-1;m>=0;m--)
+                {
+                  xjjroot::setthgrstyle((hSignalXnorm.at(k))[(m*nRefBins+l)*nPtBins+i], amcolor[m], amstyle[l], 1.2, amcolor[m], 1, 1, -1, -1, -1);
+                  (hSignalXnorm.at(k))[(m*nRefBins+l)*nPtBins+i]->Draw("pe same");
+                  if(!l) leg->AddEntry((hSignalXnorm.at(k))[(m*nRefBins+l)*nPtBins+i], legCases[m].Data(), "p");
+                  else legref->AddEntry((hSignalXnorm.at(k))[(m*nRefBins+l)*nPtBins+i], m==(nCases-1)?"#eta reflection":"", "p");
+                }
+            }
+          xjjroot::drawCMS(collisionsyst);
+          Float_t texxpos = 0.22, texypos = 0.85, texdypos = 0.06;
+          texypos += texdypos;
+          for(std::vector<TString>::const_iterator it=vectex.begin(); it!=vectex.end(); it++)
+            xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), *it);
+          TString tpt = ptBins[i+1]>=999?Form("p_{T}^{D} > %s GeV/c",xjjc::number_remove_zero(ptBins[i]).c_str()):Form("%s < p_{T}^{D} < %s GeV/c",xjjc::number_remove_zero(ptBins[i]).c_str(),xjjc::number_remove_zero(ptBins[i+1]).c_str());
+          xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), tpt.Data());
+          if(k) xjjroot::drawtex(texxpos, texypos=(texypos-texdypos), "#DeltaR < 0.3");
+          leg->Draw();
+          legref->Draw();
+
+          c->cd();
+          TPad* pPull = new TPad("pPull", "", 0, 0, 1, yPullpaddiv);
+          pPull->SetMargin(xjjroot::margin_pad_left, xjjroot::margin_pad_right, 0.07*1/yPullpaddiv, 0);
+          pPull->Draw();
+          pPull->cd();
+          TH2F* hemptyPull = new TH2F("hemptyPull", Form(";%s;%s",xtitle[k].Data(),"Ratio"), 5, vxBins[k].front(), vxBins[k].back(), 10, yPullaxismin[k], yPullaxismax[k]);
+          hemptyPull->GetXaxis()->SetNdivisions(505);
+          hemptyPull->GetYaxis()->SetNdivisions(504);
+          xjjroot::sethempty(hemptyPull, -0.5, -0);
+          hemptyPull->GetYaxis()->SetTitleSize(hemptyPull->GetYaxis()->GetTitleSize() * (ypaddiv / yPullpaddiv));
+          hemptyPull->GetXaxis()->SetTitleSize(hemptyPull->GetXaxis()->GetTitleSize() * (ypaddiv / yPullpaddiv));
+          hemptyPull->GetYaxis()->SetLabelSize(hemptyPull->GetYaxis()->GetLabelSize() * (ypaddiv / yPullpaddiv));
+          hemptyPull->GetXaxis()->SetLabelSize(hemptyPull->GetXaxis()->GetLabelSize() * (ypaddiv / yPullpaddiv));
+          hemptyPull->GetYaxis()->SetTitleOffset(hemptyPull->GetYaxis()->GetTitleOffset() / (ypaddiv / yPullpaddiv));
+          hemptyPull->GetXaxis()->SetTitleOffset(hemptyPull->GetXaxis()->GetTitleOffset() / (ypaddiv / yPullpaddiv) *(1+1.8));
+          hemptyPull->GetYaxis()->SetLabelOffset(hemptyPull->GetYaxis()->GetLabelOffset() / (ypaddiv / yPullpaddiv) *(1+12));
+          hemptyPull->GetXaxis()->SetLabelOffset(hemptyPull->GetXaxis()->GetLabelOffset() / (ypaddiv / yPullpaddiv));
+          hemptyPull->Draw();
+          xjjroot::drawbox(vxBins[k].front(), 0.90, vxBins[k].back(), 1.10, kGray, 0.4);
+          xjjroot::drawline(vxBins[k].front(), 1.00, vxBins[k].back(), 1.00, kGray+2, 2, 4);
+          for(int l=0;l<nRefBins;l++)
+            {
+              if(l) continue;
+              for(int m=0;m<nCases;m++)
+                {
+                  if(m==nCases-1) continue;
+                  xjjroot::setthgrstyle((hSignalXnormPull.at(k))[(m*nRefBins+l)*nPtBins+i], amcolor[m], amstyle[l], 1.2, amcolor[m], 1, 1, -1, -1, -1);
+                  (hSignalXnormPull.at(k))[(m*nRefBins+l)*nPtBins+i]->Draw("pe same");
+                }
+            }
+          c->SaveAs(Form("plotclosure/cclosure_xsec_%s_%s_pt_%s_%s.pdf",outputname.Data(),tname[k].Data(),xjjc::number_to_string(ptBins[i]).c_str(),xjjc::number_to_string(ptBins[i+1]).c_str()));
+
+          delete leg;
+          delete hemptyPull;
+          delete hempty;
+          delete pPull;
+          delete pXsec;
+          delete c;
+        }
+    }
+
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc==(nCases+6))
+    {
+      std::vector<TString> inputhistname;
+      for(int m=0;m<nCases;m++) inputhistname.push_back(argv[m+1]);
+      djtclosure_plothist(inputhistname, argv[nCases+1], argv[nCases+2], atof(argv[nCases+3]), atof(argv[nCases+4]), atof(argv[nCases+5]));
+      return 0;
+    }
+  else
+    {
+      std::cout<<"  Error: invalid arguments number - djtclosure_plothist()"<<std::endl;
+      return 1;
+    }
+}
+
+int arguerr(std::vector<TString>inputhistname, TString collisionsyst, Int_t plotref)
+{
+  if(inputhistname.size()!=nCases)
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"inputhistname\""<<std::endl;
+      return 1;
+    } 
+  if(collsyst_list.find(collisionsyst)==collsyst_list.end())
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"collisionsyst\""<<std::endl;
+      return 1;
+    } 
+  if(plotref!=0 && plotref!=1)
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"plotref\""<<std::endl;
+      return 1;
+    }
+  return 0;
+}
+
