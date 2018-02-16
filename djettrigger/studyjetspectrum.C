1d33e295267721687d24faa9afeb262ea09a111a
Gian Michele Innocenti <ginnocen@cern.ch>
2018-02-16 14:05:33 -0500

    adding L1 and HLT turn on curves

--- djettrigger/studyjetspectrum.C
+++ djettrigger/studyjetspectrum.C
@@ -13,8 +13,7 @@
 #include <TROOT.h>
 #include <TStyle.h>
 #include <TCanvas.h>
-
-
+#include <TGraphAsymmErrors.h>
 
 void studyjetspectrum(){
 
@@ -35,7 +34,14 @@ void studyjetspectrum(){
 	TFile *finput[samples];
     TH1F *hjetptspectrum[samples][nCases];  
     TH1F *hjetleadingptspectrum[samples][nCases];  
+    
+    TH1F *hL1efficiencyden[samples][nCases];  
+    TH1F *hL1efficiencynum[samples][nCases];  
+    TH1F *hHLTefficiencyden[samples][nCases];  
+    TH1F *hHLTefficiencynum[samples][nCases];  
 
+    TGraphAsymmErrors *gL1efficiency[samples][nCases];  
+    TGraphAsymmErrors *gHLTefficiency[samples][nCases];  
 
 	for (int index=0;index<samples;index++){
 		finput[index]=new TFile(namefiles[index].Data(),"read"); 
@@ -43,23 +49,63 @@ void studyjetspectrum(){
 		TTree*ttemp=(TTree*)finput[index]->Get(nametree[index].Data());
 		TTree*ttempHLT=(TTree*)finput[index]->Get(nametreeHLT[index].Data());
 		ttemp->AddFriend(ttempHLT);
+		
 		for (int indexcases=0;indexcases<nCases;indexcases++){ 
+
 		  ttemp->Draw(Form("%s>>htemp",namevariable[index].Data()),nametriggerselection[index][indexcases].Data());
 		  hjetptspectrum[index][indexcases]=(TH1F*)htemp->Clone();
 		  hjetptspectrum[index][indexcases]->SetName(namehjetptspectrum[index][indexcases].Data());
+		  
 		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),nametriggerselection[index][indexcases].Data());
 		  hjetleadingptspectrum[index][indexcases]=(TH1F*)htemp->Clone();
 		  hjetleadingptspectrum[index][indexcases]->SetName(namehjetleadingptspectrum[index][indexcases].Data());
+	    }
+	    
+		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
+		
+		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),MBselection[index].Data());
+		  hL1efficiencyden[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hL1efficiencyden[index][indextriggers]->SetName(namehL1efficiencyden[index][indextriggers].Data());
+	  
+	      cout<<"index="<<index<<endl;
+		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),Form("%s&&%s",MBselection[index].Data(),nameL1trigger[index][indextriggers].Data()));
+		  hL1efficiencynum[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hL1efficiencynum[index][indextriggers]->SetName(namehL1efficiencynum[index][indextriggers].Data());
+
+          gL1efficiency[index][indextriggers] = new TGraphAsymmErrors;
+          gL1efficiency[index][indextriggers]->BayesDivide(hL1efficiencynum[index][indextriggers],hL1efficiencyden[index][indextriggers]);
+          gL1efficiency[index][indextriggers]->SetName(namegL1efficiency[index][indextriggers].Data());
+
+		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),MBselection[index].Data());
+		  hHLTefficiencyden[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hHLTefficiencyden[index][indextriggers]->SetName(namehHLTefficiencyden[index][indextriggers].Data());
+
+		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),Form("%s&&%s",MBselection[index].Data(),nametrigger[index][indextriggers].Data()));
+		  hHLTefficiencynum[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hHLTefficiencynum[index][indextriggers]->SetName(namehHLTefficiencynum[index][indextriggers].Data());
+
+          gHLTefficiency[index][indextriggers] = new TGraphAsymmErrors;
+          gHLTefficiency[index][indextriggers]->BayesDivide(hHLTefficiencynum[index][indextriggers],hHLTefficiencyden[index][indextriggers]);
+          gHLTefficiency[index][indextriggers]->SetName(namegHLTefficiency[index][indextriggers].Data());
 		}
 	}
 	
 	TFile*foutput=new TFile("foutput.root","recreate");
 	foutput->cd();
+	
 	for (int index=0;index<samples;index++){
 		for (int indexcases=0;indexcases<nCases;indexcases++){
 		  hjetptspectrum[index][indexcases]->Write();
 		  hjetleadingptspectrum[index][indexcases]->Write();
 		}
+		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
+		  hL1efficiencyden[index][indextriggers]->Write();
+		  hL1efficiencynum[index][indextriggers]->Write();
+		  hHLTefficiencyden[index][indextriggers]->Write();
+		  hHLTefficiencynum[index][indextriggers]->Write();
+		  gL1efficiency[index][indextriggers]->Write();
+		  gHLTefficiency[index][indextriggers]->Write();
+        }
     }
 }
 
--- djettrigger/utilitiesjetspectrum.h
+++ djettrigger/utilitiesjetspectrum.h
@@ -2,16 +2,32 @@
   const int nCases=5;
   const int ntriggers=2;
 
+
+  //nCases
   TString labelsamples[samples]={"pp","PbPb"};  
+  TString MBselection[samples]={"(HLT_L1MinimumBiasHF1OR_part0_v1||HLT_L1MinimumBiasHF1OR_part1_v1||HLT_L1MinimumBiasHF1OR_part2_v1||HLT_L1MinimumBiasHF1OR_part3_v1||HLT_L1MinimumBiasHF1OR_part4_v1||HLT_L1MinimumBiasHF1OR_part5_v1||HLT_L1MinimumBiasHF1OR_part6_v1||HLT_L1MinimumBiasHF1OR_part7_v1||HLT_L1MinimumBiasHF1OR_part8_v1||HLT_L1MinimumBiasHF1OR_part9_v1||HLT_L1MinimumBiasHF1OR_part10_v1||HLT_L1MinimumBiasHF1OR_part11_v1||HLT_L1MinimumBiasHF1OR_part12_v1||HLT_L1MinimumBiasHF1OR_part13_v1||HLT_L1MinimumBiasHF1OR_part14_v1||HLT_L1MinimumBiasHF1OR_part15_v1||HLT_L1MinimumBiasHF1OR_part16_v1||HLT_L1MinimumBiasHF1OR_part17_v1||HLT_L1MinimumBiasHF1OR_part18_v1||HLT_L1MinimumBiasHF1OR_part19_v1)","(HLT_HIL1MinimumBiasHF2AND_part1_v1 || HLT_HIL1MinimumBiasHF2AND_part2_v1 || HLT_HIL1MinimumBiasHF2AND_part3_v1)"};  
   TString namefiles[samples]={"/export/d00/scratch/jwang/Djets/data/DjetFiles_20171120_pp_5TeV_HighPtLowerJets_dPt4tkPt1p5Alpha0p2Decay2_D0Dstar_20170614.root","/export/d00/scratch/jwang/Djets/data/DjetFiles_20171120_PbPb_5TeV_HIHardProbes_skimmed_1unit_part1234_26March_20170326_HLTHIPuAK4CaloJet406080.root"}; 
   TString nametree[samples]={"djt","djt"};
   TString nametreeHLT[samples]={"hlt","hlt"};
   TString namevariable[samples]={"jetptCorr_akpu3pf","jetptCorr_akpu3pf"};
-  TString nametrigger[samples][ntriggers]={{"HLT_AK4PFJet40_Eta5p1_v1","HLT_AK4PFJet60_Eta5p1_v1"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1","HLT_HIPuAK4CaloJet60_Eta5p1_v1"}};
   TString nametriggerselection[samples][nCases]={{"HLT_AK4PFJet40_Eta5p1_v1&&!HLT_AK4PFJet60_Eta5p1_v1","!HLT_AK4PFJet40_Eta5p1_v1&&HLT_AK4PFJet60_Eta5p1_v1","HLT_AK4PFJet40_Eta5p1_v1&&HLT_AK4PFJet60_Eta5p1_v1","HLT_AK4PFJet40_Eta5p1_v1||HLT_AK4PFJet60_Eta5p1_v1","1"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1&&!HLT_HIPuAK4CaloJet60_Eta5p1_v1", "!HLT_HIPuAK4CaloJet40_Eta5p1_v1&&HLT_HIPuAK4CaloJet60_Eta5p1_v1","HLT_HIPuAK4CaloJet40_Eta5p1_v1&&HLT_HIPuAK4CaloJet60_Eta5p1_v1","HLT_HIPuAK4CaloJet40_Eta5p1_v1||HLT_HIPuAK4CaloJet60_Eta5p1_v1","1"}};
   TString nametriggerselectiontag[samples][nCases]={{"HLT40only","HLT60only","HLT40_AND_HLT60","HLT40_OR_HLT60","All"},{"HLT40only","HLT60only","HLT40_AND_HLT60","HLT40_OR_HLT60","All"}};
   TString namehjetptspectrum[samples][nCases];  
   TString namehjetleadingptspectrum[samples][nCases];  
+  
+  //nTriggers  
+  TString nametrigger[samples][ntriggers]={{"HLT_AK4PFJet40_Eta5p1_v1","HLT_AK4PFJet60_Eta5p1_v1"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1","HLT_HIPuAK4CaloJet60_Eta5p1_v1"}};
+  TString nameL1trigger[samples][ntriggers]={{"L1_SingleJet28_BptxAND","L1_SingleJet40_BptxAND"},{"L1_MinimumBiasHF2_AND","L1_SingleS1Jet28_BptxAND"}};
+  TString nametriggerPrescl[samples][ntriggers]={{"HLT_AK4PFJet40_Eta5p1_v1_Prescl","HLT_AK4PFJet60_Eta5p1_v1_Prescl"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1_Prescl","HLT_HIPuAK4CaloJet60_Eta5p1_v1_Prescl"}};
+  TString nameL1triggerPresc[samples][ntriggers]={{"L1_SingleJet28_BptxAND_Prescl","L1_SingleJet40_BptxAND_Prescl"},{"L1_MinimumBiasHF2_AND_Prescl","L1_SingleS1Jet28_BptxAND_Prescl"}}; 
+  
+  TString namehL1efficiencyden[samples][ntriggers];  
+  TString namehL1efficiencynum[samples][ntriggers];  
+  TString namehHLTefficiencyden[samples][ntriggers];  
+  TString namehHLTefficiencynum[samples][ntriggers];  
+  
+  TString namegL1efficiency[samples][ntriggers];  
+  TString namegHLTefficiency[samples][ntriggers];  
 
   int colours[nCases]={1,2,3,4,7};
   int markerstyle[nCases]={21,22,23,24,24};
@@ -26,12 +42,19 @@
   TString string_xaxis[samples]={"jet offline p_{T}","jet offline p_{T}"};
   TString string_yaxis[samples]={"entries","entries"};
 
-
 void initialise(){
 	for (int index=0;index<samples;index++){
 		for (int indexcases=0;indexcases<nCases;indexcases++){ 
 		   namehjetptspectrum[index][indexcases]="hjetptspectrum"+nametriggerselectiontag[index][indexcases]+labelsamples[index];
 		   namehjetleadingptspectrum[index][indexcases]="hjetleadingptspectrum"+nametriggerselectiontag[index][indexcases]+labelsamples[index];
+        }
+		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
+		   namehL1efficiencyden[index][indextriggers]="hL1efficiencyden"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
+		   namehL1efficiencynum[index][indextriggers]="hL1efficiencynum"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
+		   namehHLTefficiencyden[index][indextriggers]="hHLTefficiencyden"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
+		   namehHLTefficiencynum[index][indextriggers]="hHLTefficiencynum"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
+		   namegL1efficiency[index][indextriggers]="gL1efficiency"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
+		   namegHLTefficiency[index][indextriggers]="gHLTefficiency"+nametriggerselectiontag[index][indextriggers]+labelsamples[index];
     }
   }
 }
