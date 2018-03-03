186a144d85f8d0a3b61137a093a41be049b0867f
Gian Michele Innocenti <ginnocen@cern.ch>
2018-03-03 09:36:25 -0500

    adding trigger correciton partial

--- djettrigger/jetspectrumCorrected.C
+++ djettrigger/jetspectrumCorrected.C
@@ -14,32 +14,36 @@
 #include <TROOT.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <TCut.h>
 #include <TGraphAsymmErrors.h>
 
-void jetspectrumCorrected(int doPP=1, int doPbPb=1, int do40=1,int do60=1,int do80=1){
+void jetspectrumCorrected(int doPP=1, int doPbPb=0, int do40=0,int do60=1,int do80=0,int do100=0){
 
     initialise();
 
     TFile *finput[samples];
+    TF1*fitErfL1[samples][ntriggers];  
+    TF1*fitErfHLT[samples][ntriggers];  
+    
+	TFile*fFit=new TFile("TurnOnCurves.root","read");
+	fFit->cd();
+	for (int index=0;index<samples;index++){
+		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
+		  fitErfL1[index][indextriggers]=(TF1*)fFit->Get(Form("fitErfL1_%s",nametriggerselectiontagtriggers[index][indextriggers].Data()));
+		  fitErfHLT[index][indextriggers]=(TF1*)fFit->Get(Form("fitErfHLT_%s",nametriggerselectiontagtriggers[index][indextriggers].Data()));
+		}
+    }
 
-    TH1F *hjetptspectrumTotPresclCorr[samples];
-    TH1F *hjetleadingptspectrumTotPresclCorr[samples];  
     TH1F *hjetptspectrumpertrigger[samples][ntriggers];  
-    TH1F *hjetleadingptspectrumpertrigger[samples][ntriggers];  
     TH1F *hjetptspectrumpertriggerPresclCorr[samples][ntriggers];  
+    TH1F *hjetptspectrumpertriggerEffWeighted[samples][ntriggers];  
+
+    TH1F *hjetleadingptspectrumpertrigger[samples][ntriggers];  
     TH1F *hjetleadingptspectrumpertriggerPresclCorr[samples][ntriggers];  
-    
-      TH1F*htemp=new TH1F("htemp","htemp",2000,0,1000);
+    TH1F *hjetleadingptspectrumpertriggerEffWeighted[samples][ntriggers];  
 
-    
-	for (int index=0;index<samples;index++) { 
-      TH1F*htemp=new TH1F("htemp","htemp",2000,0,1000);
-	  hjetptspectrumTotPresclCorr[index]=(TH1F*)htemp->Clone(); 
-	  hjetptspectrumTotPresclCorr[index]->SetName(namehjetptspectrumTotPresclCorr[index].Data());
-	  hjetleadingptspectrumTotPresclCorr[index]=(TH1F*)htemp->Clone(); 
-	  hjetleadingptspectrumTotPresclCorr[index]->SetName(namehjetleadingptspectrumTotPresclCorr[index].Data());
-	}
-
+    TH1F*htemp=new TH1F("htemp","htemp",2000,0,1000);
+ 
 	for (int index=0;index<samples;index++){
 
 		if (index==0 && doPP==0) continue;
@@ -57,8 +61,13 @@ void jetspectrumCorrected(int doPP=1, int doPbPb=1, int do40=1,int do60=1,int do
 		  if (indextriggers==0 && do40==0) continue;
 		  if (indextriggers==1 && do60==0) continue;
 		  if (indextriggers==2 && do80==0) continue;
+		  if (indextriggers==3 && do100==0) continue;
 		  
 		  cout<<"selectionanalysis"<<selectionanalysis[index][indextriggers].Data()<<endl;
+		  cout<<"selectionanalysisweightrange"<<selectionanalysisweightrange[index][indextriggers].Data()<<endl;
+
+          TString l1weight=Form(stringweight.Data(),fitErfL1[index][indextriggers]->GetParameter(0),fitErfL1[index][indextriggers]->GetParameter(1),fitErfL1[index][indextriggers]->GetParameter(1),fitErfL1[index][indextriggers]->GetParameter(3),fitErfL1[index][indextriggers]->GetParameter(2));
+          TString hltweight=Form(stringweight.Data(),fitErfHLT[index][indextriggers]->GetParameter(0),fitErfHLT[index][indextriggers]->GetParameter(1),fitErfHLT[index][indextriggers]->GetParameter(1),fitErfHLT[index][indextriggers]->GetParameter(3),fitErfHLT[index][indextriggers]->GetParameter(2));
 
 		  ttemp->Draw(Form("%s>>htemp",namevariable[index].Data()),selectionanalysis[index][indextriggers].Data());
 		  hjetptspectrumpertrigger[index][indextriggers]=(TH1F*)htemp->Clone();
@@ -68,6 +77,14 @@ void jetspectrumCorrected(int doPP=1, int doPbPb=1, int do40=1,int do60=1,int do
 		  hjetleadingptspectrumpertrigger[index][indextriggers]=(TH1F*)htemp->Clone();
 		  hjetleadingptspectrumpertrigger[index][indextriggers]->SetName(namehjetleadingptspectrumpertrigger[index][indextriggers].Data());
 		  
+		  ttemp->Draw(Form("%s>>htemp",namevariable[index].Data()),selectionanalysisweightrange[index][indextriggers].Data()*TCut(l1weight)*TCut(hltweight));
+		  hjetptspectrumpertriggerEffWeighted[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hjetptspectrumpertriggerEffWeighted[index][indextriggers]->SetName(namehjetptspectrumpertriggerEffWeighted[index][indextriggers].Data());
+
+		  ttemp->Draw(Form("Max$(%s)>>htemp",namevariable[index].Data()),selectionanalysisweightrange[index][indextriggers].Data()*TCut(l1weight)*TCut(hltweight));
+		  hjetleadingptspectrumpertriggerEffWeighted[index][indextriggers]=(TH1F*)htemp->Clone();
+		  hjetleadingptspectrumpertriggerEffWeighted[index][indextriggers]->SetName(namehjetleadingptspectrumpertriggerEffWeighted[index][indextriggers].Data());
+
 		  hjetptspectrumpertriggerPresclCorr[index][indextriggers]=(TH1F*)hjetptspectrumpertrigger[index][indextriggers]->Clone();
 		  hjetptspectrumpertriggerPresclCorr[index][indextriggers]->SetName(namehjetptspectrumpertriggerPresclCorr[index][indextriggers].Data());
 		  hjetptspectrumpertriggerPresclCorr[index][indextriggers]->Sumw2();
@@ -77,10 +94,7 @@ void jetspectrumCorrected(int doPP=1, int doPbPb=1, int do40=1,int do60=1,int do
 		  hjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]->SetName(namehjetleadingptspectrumpertriggerPresclCorr[index][indextriggers].Data());
 		  hjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]->Sumw2();
 		  hjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]->Scale(1./dataluminosity[index][indextriggers]);
-		  
-		  hjetptspectrumTotPresclCorr[index]->Add(hjetptspectrumpertriggerPresclCorr[index][indextriggers]);
-		  hjetleadingptspectrumTotPresclCorr[index]->Add(hjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]);
-		
+		  		
 		}
 	}
 
@@ -92,19 +106,20 @@ void jetspectrumCorrected(int doPP=1, int doPbPb=1, int do40=1,int do60=1,int do
 		if (index==0 && doPP==0) continue;
 		if (index==1 && doPbPb==0) continue;
 		
-		hjetptspectrumTotPresclCorr[index]->Write();
-		hjetleadingptspectrumTotPresclCorr[index]->Write();
-
 		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
 
 	   	  if (indextriggers==0 && do40==0) continue;
 		  if (indextriggers==1 && do60==0) continue;
 		  if (indextriggers==2 && do80==0) continue;
-		  
+		  if (indextriggers==3 && do100==0) continue;
+
 		  hjetptspectrumpertrigger[index][indextriggers]->Write();
 		  hjetleadingptspectrumpertrigger[index][indextriggers]->Write();
 		  hjetptspectrumpertriggerPresclCorr[index][indextriggers]->Write();
 		  hjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]->Write();
+		  hjetptspectrumpertriggerEffWeighted[index][indextriggers]->Write();
+		  hjetleadingptspectrumpertriggerEffWeighted[index][indextriggers]->Write();
+
 		}
     }
     foutput->Close();
--- djettrigger/utilitiescorrectedspectrum.h
+++ djettrigger/utilitiescorrectedspectrum.h
@@ -14,15 +14,19 @@
   TString namefiles[samples]={"/mnt/T2_US_MIT/submit-hi2/scratch/jwang/Djets/data/DjetFiles_20180214_pp_5TeV_HighPtLowerJetsHighPtJet80_dPt4tkPt1p5Alpha0p2Decay2_D0Dstar_20170614.root","/mnt/T2_US_MIT/submit-hi2/scratch/jwang/Djets/data/DjetFiles_20171120_PbPb_5TeV_HIHardProbes_skimmed_1unit_part1234_26March_20170326_HLTHIPuAK4CaloJet406080.root"}; 
   TString nametree[samples]={"djt","djt"};
   TString nametreeHLT[samples]={"hlt","hlt"};
-  TString namevariable[samples]={"jetptCorr_akpu3pf","jetptCorr_akpu3pf"};
-  TString nametrigger[samples][ntriggers]={{"fileno==0&&HLT_AK4PFJet40_Eta5p1_v1","fileno==0&&HLT_AK4PFJet60_Eta5p1_v1","fileno==1&&HLT_AK4PFJet80_Eta5p1_v1","fileno==1&&HLT_AK4PFJet100_Eta5p1_v1"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1","HLT_HIPuAK4CaloJet60_Eta5p1_v1","HLT_HIPuAK4CaloJet80_Eta5p1_v1","HLT_HIPuAK4CaloJet100_Eta5p1_v1"}};
- TString nameL1trigger[samples][ntriggers]={{"L1_SingleJet28_BptxAND","L1_SingleJet40_BptxAND","L1_SingleJet48_BptxAND","L1_SingleJet52_BptxAND"},{"L1_MinimumBiasHF2_AND","L1_SingleS1Jet28_BptxAND","L1_SingleJet44_BptxAND","L1_SingleS1Jet56_BptxAND"}};
+  TString namevariable[samples]={"jetpt_akpu3pf","jetpt_akpu3pf"};
+  
+  TString stringweight="1./(0.5*TMath::Erf((jetpt_akpu3pf[0])*%f+%f+TMath::Exp((-(jetpt_akpu3pf[0])^2+%f)/(%f))*(%f))+0.5)";  //[0],[1],[1],[3],[2]
+  TString etaselection="abs(jeteta_akpu3pf)<1.6";
+  TString nametrigger[samples][ntriggers]={{"fileno==0&&HLT_AK4PFJet40_Eta5p1_v1&&jetpt_akpu3pf>40","fileno==0&&HLT_AK4PFJet60_Eta5p1_v1&&jetpt_akpu3pf>60","fileno==1&&HLT_AK4PFJet80_Eta5p1_v1&&jetpt_akpu3pf>80","fileno==1&&HLT_AK4PFJet100_Eta5p1_v1&&jetpt_akpu3pf>100"},{"HLT_HIPuAK4CaloJet40_Eta5p1_v1&&jetpt_akpu3pf>40","HLT_HIPuAK4CaloJet60_Eta5p1_v1&&jetpt_akpu3pf>60","HLT_HIPuAK4CaloJet80_Eta5p1_v1&&jetpt_akpu3pf>80","HLT_HIPuAK4CaloJet100_Eta5p1_v1&&jetpt_akpu3pf>100"}};
+  TString triggerrangecorrectionsel[samples][ntriggers]={{"abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>40)","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>60","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>80","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>100"},{"abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>40)","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>60","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>80","abs(jeteta_akpu3pf[0])<1.6&&jetpt_akpu3pf[0]>100"}};
+  
   TString namehjetptspectrumpertrigger[samples][ntriggers];  
-  TString namehjetleadingptspectrumpertrigger[samples][ntriggers];  
   TString namehjetptspectrumpertriggerPresclCorr[samples][ntriggers];  
+  TString namehjetptspectrumpertriggerEffWeighted[samples][ntriggers];  
+  TString namehjetleadingptspectrumpertrigger[samples][ntriggers];  
   TString namehjetleadingptspectrumpertriggerPresclCorr[samples][ntriggers];  
-  TString namehjetptspectrumTotPresclCorr[samples];  
-  TString namehjetleadingptspectrumTotPresclCorr[samples];  
+  TString namehjetleadingptspectrumpertriggerEffWeighted[samples][ntriggers];  
 
   double nbins[samples]={2000,2000};
   double lowerrangex[samples]={20.,20.};
@@ -34,6 +38,8 @@
   TString string_yaxis[samples]={"entries","entries"};
   
   TString selectionanalysis[samples][ntriggers];
+  TString selectionanalysisweightrange[samples][ntriggers];
+
 
 void initialise(){
 
@@ -52,17 +58,16 @@ void initialise(){
         
     
 	for (int index=0;index<samples;index++){
-	
-		   namehjetptspectrumTotPresclCorr[index]="hjetptspectrumTotPresclCorr"+labelsamples[index];
-		   namehjetleadingptspectrumTotPresclCorr[index]="hjetleadingptspectrumTotPresclCorr"+labelsamples[index];
-
 		for (int indextriggers=0;indextriggers<ntriggers;indextriggers++){ 
 		
-		   namehjetptspectrumpertriggerPresclCorr[index][indextriggers]="hjetptspectrumpertriggerPresclCorr"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
-		   namehjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]="hjetleadingptspectrumpertriggerPresclCorr"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
 		   namehjetptspectrumpertrigger[index][indextriggers]="hjetptspectrumpertrigger"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
+		   namehjetptspectrumpertriggerPresclCorr[index][indextriggers]="hjetptspectrumpertriggerPresclCorr"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
+		   namehjetptspectrumpertriggerEffWeighted[index][indextriggers]="hjetptspectrumpertriggerEffWeighted"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
 		   namehjetleadingptspectrumpertrigger[index][indextriggers]="hjetleadingptspectrumpertrigger"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];	
-		   selectionanalysis[index][indextriggers]=nametrigger[index][indextriggers]+"&&abs(jeteta_akpu3pf)<2.0";
+		   namehjetleadingptspectrumpertriggerPresclCorr[index][indextriggers]="hjetleadingptspectrumpertriggerPresclCorr"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];
+		   namehjetleadingptspectrumpertriggerEffWeighted[index][indextriggers]="hjetleadingptspectrumpertriggerEffWeighted"+nametriggerselectiontagtriggers[index][indextriggers]+labelsamples[index];	
+		   selectionanalysis[index][indextriggers]=nametrigger[index][indextriggers]+"&&"+etaselection;
+		   selectionanalysisweightrange[index][indextriggers]=nametrigger[index][indextriggers]+"&&"+etaselection+"&&"+triggerrangecorrectionsel[index][indextriggers];
     }
   }
 }
