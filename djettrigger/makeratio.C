d05a9656160544f717012a05a29e92f1aed268bf
Michael Peters <mjpianist.000@gmail.com>
2018-03-12 21:25:02 -0400

    Added HLT60/HLT40 ratio

--- djettrigger/makeratio.C
+++ djettrigger/makeratio.C
@@ -13,8 +13,10 @@ void makeratio()
 
 	const int nplots = 1;
 
+    TH1F *hjetleadingptspectrumpertriggerPresclCorr[samples][ntriggers];
     TH1F *hjetleadingptspectrumpertriggerPresclCorrEffWeighted[samples][ntriggers];
 
+    TH1F *hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[samples];
     TH1F *hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[samples];
     TH1F *hjetleadingptspectrumpertriggerPresclCorrEffWeighted10080ratio[samples];
 
@@ -27,12 +29,17 @@ void makeratio()
     	{
             if(!plotturnon[i][j]) continue;
             std::cout << namehjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][j] << std::endl;
+            hjetleadingptspectrumpertriggerPresclCorr[i][j] = (TH1F*)fin->Get(namehjetleadingptspectrumpertriggerPresclCorr[i][j].Data());
             hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][j] = (TH1F*)fin->Get(namehjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][j].Data());
     	}
+        hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i] = (TH1F*)hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][1]->Clone();
+        hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Sumw2();
     	hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[i] = (TH1F*)hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][1]->Clone();
     	hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[i]->Sumw2();
     	hjetleadingptspectrumpertriggerPresclCorrEffWeighted10080ratio[i] = (TH1F*)hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][1]->Clone();
     	hjetleadingptspectrumpertriggerPresclCorrEffWeighted10080ratio[i]->Sumw2();
+        // For 60/40 ratio, divide by _uncorrected_ HLT40
+        if(plotturnon[i][1]==1 && plotturnon[i][0]==1) hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Divide(hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][1],hjetleadingptspectrumpertriggerPresclCorr[i][0]);
     	if(plotturnon[i][2]==1 && plotturnon[i][1]==1) hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[i]->Divide(hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][2],hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][1]);
     	if(plotturnon[i][3]==1 && plotturnon[i][2]==1) hjetleadingptspectrumpertriggerPresclCorrEffWeighted10080ratio[i]->Divide(hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][3],hjetleadingptspectrumpertriggerPresclCorrEffWeighted[i][2]);
     }
@@ -47,8 +54,7 @@ void makeratio()
     	for(int k=0;k<nplots;k++)
     	{
 
-    		if(i%2==0) hempties[k][i] = new TH2F("",Form("%s;leading p_{t};Ratio",titles[k].c_str()),1000,0,300,1000,0,2);
-    		if(i%2==1) hempties[k][i] = new TH2F("",Form("%s;leading p_{t};Ratio",titles[k].c_str()),1000,0,300,1000,0,2);
+    		hempties[k][i] = new TH2F("",Form("%s;leading p_{t};Ratio",titles[k].c_str()),1000,0,300,1000,0,2);
     		c[k][i] = new TCanvas(Form("%d%d",k,i),"",800,800);
     		xjjroot::sethempty(hempties[k][i],0,0);
     		hempties[k][i]->Draw();
@@ -56,8 +62,17 @@ void makeratio()
     	}
 
     	c[0][i]->cd();
+        xjjroot::setthgrstyle(hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i],kRed,20,1.2,kRed,-1,1,-1,0.1,-1);
     	xjjroot::setthgrstyle(hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[i],kBlack,20,1.2,kBlack,-1,1,-1,0.1,-1);
     	xjjroot::setthgrstyle(hjetleadingptspectrumpertriggerPresclCorrEffWeighted10080ratio[i],kBlue,20,1.2,kBlue,-1,1,-1,0.1,-1);
+        if(plotturnon[i][1]==1 && plotturnon[i][0]==1)
+        {
+            hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Sumw2();
+            hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Rebin(5);
+            hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Scale(1./5);
+            hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i]->Draw("PE same");
+            leg[0][i]->AddEntry(hjetleadingptspectrumpertriggerPresclCorrEffWeighted6040ratio[i],"HLT60/HLT40","lp");
+        }
 	    if(plotturnon[i][2]==1 && plotturnon[i][1]==1) 
 	    {
 	    	hjetleadingptspectrumpertriggerPresclCorrEffWeighted8060ratio[i]->Sumw2();
