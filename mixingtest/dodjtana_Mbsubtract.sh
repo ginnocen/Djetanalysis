4887ae332c41e82f96bd6cc5d3e7438a78b3153d
Gian Michele Innocenti <ginnocen@cern.ch>
2018-03-29 13:32:45 -0400

    cleean old doanalysis

--- mixingtest/dodjtana_Mbsubtract.sh
+++ mixingtest/dodjtana_Mbsubtract.sh
@@ -14,8 +14,6 @@ then
 fi
 
 DOMIX=${1:-0}
-MAKEHISTS=${2:-0}
-DOMBSUB=${3:-0}
 
 #DJETPATH="~/mbfile/T2_US_MIT/submit-hi2/scratch/jwang/Djets/MC"
 #WORKPATH="~/mbfile/T2_US_MIT/submit-hi2/scratch/mjpeters/Djet/MC"
@@ -37,46 +35,4 @@ then
     g++ MB_D_jet_mix_fast.C $(root-config --cflags --libs) -Wall -O2 -o MB_D_jet_mix_fast.exe
     ./MB_D_jet_mix_fast.exe "${DJETPATH}/${DJETFILE}.root" "${MBPATH}/${MBJETFILE}.root" "${WORKPATH}/${MBMIXFILE}.root" $ISMC $ISPP
     rm MB_D_jet_mix_fast.exe
-fi
-
-if [[ $MAKEHISTS -eq 1 ]]
-then
-    FILES=("${MBMIXFILE}signal" "gen_${MBMIXFILE}" "mixD_${MBMIXFILE}" "mixjet_${MBMIXFILE}" "mixboth_${MBMIXFILE}")
-    cd plots
-    for i in ${FILES[@]}
-    do
-        if [[ ! -d $i ]]
-        then
-            mkdir -p $i
-        fi
-    done
-    cd ..
-    if [[ $ISPP -eq 0 ]]
-    then
-        COLL="PbPb"
-    else
-        COLL="pp"
-    fi
-    g++ djtana_savetpl_corr.C $(root-config --cflags --libs) -Wall -O2 -o djtana_savetpl_corr.exe
-    g++ djtana_plothist.C $(root-config --cflags --libs) -Wall -O2 -o djtana_plothist.exe
-    ./djtana_savetpl_corr.exe "${WORKPATH}/${MBMIXFILE}.root" "rootfiles/${MBMIXFILE}signal_hists" $COLL $ISMC 1 $JETPTMIN $JETETAMIN $JETETAMAX 0 
-    ./djtana_savetpl_corr.exe "${WORKPATH}/${MBMIXFILE}.root" "rootfiles/gen_${MBMIXFILE}_hists" $COLL $ISMC 3 $JETPTMIN $JETETAMIN $JETETAMAX 0 
-    ./djtana_savetpl_corr.exe "${WORKPATH}/${MBMIXFILE}.root" "rootfiles/mixD_${MBMIXFILE}_hists" $COLL $ISMC 1 $JETPTMIN $JETETAMIN $JETETAMAX 1
-    ./djtana_savetpl_corr.exe "${WORKPATH}/${MBMIXFILE}.root" "rootfiles/mixjet_${MBMIXFILE}_hists" $COLL $ISMC 1 $JETPTMIN $JETETAMIN $JETETAMAX 2
-    ./djtana_savetpl_corr.exe "${WORKPATH}/${MBMIXFILE}.root" "rootfiles/mixboth_${MBMIXFILE}_hists" $COLL $ISMC 1 $JETPTMIN $JETETAMIN $JETETAMAX 3
-    wait
-    ./djtana_plothist.exe "rootfiles/${MBMIXFILE}signal_hists" "${MBMIXFILE}signal" $COLL $ISMC $JETPTMIN $JETETAMIN $JETETAMAX &
-    ./djtana_plothist.exe "rootfiles/gen_${MBMIXFILE}_hists" "gen_${DJETFILE}" $COLL $ISMC $JETPTMIN $JETETAMIN $JETETAMAX &
-    ./djtana_plothist.exe "rootfiles/mixD_${MBMIXFILE}_hists" "mixD_${MBMIXFILE}" $COLL $ISMC $JETPTMIN $JETETAMIN $JETETAMAX &
-    ./djtana_plothist.exe "rootfiles/mixjet_${MBMIXFILE}_hists" "mixjet_${MBMIXFILE}" $COLL $ISMC $JETPTMIN $JETETAMIN $JETETAMAX &
-    ./djtana_plothist.exe "rootfiles/mixboth_${MBMIXFILE}_hists" "mixboth_${MBMIXFILE}" $COLL $ISMC $JETPTMIN $JETETAMIN $JETETAMAX
-    rm djtana_savetpl_corr.exe
-    rm djtana_plothist.exe
-fi
-
-if [[ $DOMBSUB -eq 1 ]]
-then
-    g++ MBsubtract_closure.C $(root-config --cflags --libs) -Werror -Wall -O2 -o MBsubtract_closure.exe
-    ./MBsubtract_closure.exe "rootfiles/${MBMIXFILE}signal_hists.root" "rootfiles/mixjet_${MBMIXFILE}_hists.root" "rootfiles/gen_${MBMIXFILE}_hists.root" "rootfiles/${OUTFILE}.root"
-    rm MBsubtract_closure.exe
 fi
