1caf4b52d423c296fbe2694e64939eebf7a00cd2
Gian Michele Innocenti <ginnocen@cern.ch>
2018-03-29 06:55:59 -0400

    committing schelethon for the subtraction

---
+++ studymixedbackground/djtana.h
@@ -0,0 +1,319 @@
+#ifndef _DJTANA_H_
+#define _DJTANA_H_
+
+#include "../includes/djet.h"
+#include "../includes/prefilters.h"
+#include "../includes/xjjcuti.h"
+#include "../includes/xjjrootuti.h"
+#include "../includes/dfitter.h"
+#include "../includes/paramCorr.h"
+#include <iostream>
+#include <iomanip>
+#include <TMath.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TLegend.h>
+#include <TRandom2.h>
+#include <TRandom3.h>
+
+const Float_t DZERO_MASS = 1.8649;
+
+//
+const int nRefBins = 2;
+TString tRef[nRefBins] = {"eta", "etaref"};
+
+Color_t amcolor[nPtBins] = {kBlack, kRed};
+Style_t amstyle[nRefBins][nPtBins] = {{20,     21},   {24,     25}};
+
+TH1F* ahHistoRMass[nRefBins][nPtBins][nDrBins];
+TH1F* ahHistoRMassSignal[nRefBins][nPtBins][nDrBins];
+TH1F* ahHistoRMassSwapped[nRefBins][nPtBins][nDrBins];
+TH1F* ahHistoZMass[nRefBins][nPtBins][nZBins];
+TH1F* ahHistoZMassSignal[nRefBins][nPtBins][nZBins];
+TH1F* ahHistoZMassSwapped[nRefBins][nPtBins][nZBins];
+    
+TH1F* hNjets;
+TH1F* ahREfficiency[nRefBins][nPtBins];
+TH1F* ahNumREfficiency[nRefBins][nPtBins];
+TH1F* ahDenREfficiency[nRefBins][nPtBins];
+TH1F* ahZEfficiency[nRefBins][nPtBins];
+TH1F* ahNumZEfficiency[nRefBins][nPtBins];
+TH1F* ahDenZEfficiency[nRefBins][nPtBins];
+
+TH1F* ahSignalRraw[nRefBins][nPtBins];
+TH1F* ahSignalZraw[nRefBins][nPtBins];
+TH1F* ahSignalR[nRefBins][nPtBins];
+TH1F* ahSignalZ[nRefBins][nPtBins];
+TH1F* ahSignalRnorm[nRefBins][nPtBins];
+TH1F* ahSignalZnorm[nRefBins][nPtBins];
+TH1F* ahSignalRsub[nPtBins];
+TH1F* ahSignalZsub[nPtBins];
+
+TH1F* ahSignalRnormP[2][nRefBins][nPtBins];
+TH1F* ahSignalZnormP[2][nRefBins][nPtBins];
+TH1F* ahSignalRnormRatio[nRefBins][nPtBins];
+TH1F* ahSignalZnormRatio[nRefBins][nPtBins];
+TH1F* ahSignalRsubP[2][nPtBins];
+TH1F* ahSignalZsubP[2][nPtBins];
+TH1F* ahSignalRsubRatio[nPtBins];
+TH1F* ahSignalZsubRatio[nPtBins];
+
+//
+int createhists(Option_t* option)
+{
+  TString opt  = option;
+  opt.ToLower();
+  if(opt=="savetpl")
+    {
+      hNjets = new TH1F("hNjets", "", 1, 0, 1); // ... is it necessary
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahREfficiency[l][i] = new TH1F(Form("hREfficiency_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahNumREfficiency[l][i] = new TH1F(Form("hNumREfficiency_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahDenREfficiency[l][i] = new TH1F(Form("hDenREfficiency_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahZEfficiency[l][i] = new TH1F(Form("hZEfficiency_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+              ahNumZEfficiency[l][i] = new TH1F(Form("hNumZEfficiency_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+              ahDenZEfficiency[l][i] = new TH1F(Form("hDenZEfficiency_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+              for(int j=0;j<nDrBins;j++) 
+                {
+                  ahHistoRMassSignal[l][i][j] = new TH1F(Form("hHistoRMassSignal_%s_pt_%d_dr_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+                  ahHistoRMassSwapped[l][i][j] = new TH1F(Form("hHistoRMassSwapped_%s_pt_%d_dr_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+                }
+              for(int j=0;j<nZBins;j++) 
+                {
+                  ahHistoZMassSignal[l][i][j] = new TH1F(Form("hHistoZMassSignal_%s_pt_%d_z_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+                  ahHistoZMassSwapped[l][i][j] = new TH1F(Form("hHistoZMassSwapped_%s_pt_%d_z_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+                }
+            }
+        }
+      return 0;
+    }
+  if(opt=="savehist")
+    {
+      hNjets = new TH1F("hNjets", "", 1, 0, 1);
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              for(int j=0;j<nDrBins;j++) ahHistoRMass[l][i][j] = new TH1F(Form("hHistoRMass_%s_pt_%d_dr_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+              for(int j=0;j<nZBins;j++) ahHistoZMass[l][i][j] = new TH1F(Form("hHistoZMass_%s_pt_%d_z_%d",tRef[l].Data(),i,j), ";m_{#piK} (GeV/c^{2});Entries / (5 MeV/c^{2})", 60, 1.7, 2.0);
+            }
+        }
+      return 0;
+    }
+  if(opt=="usehist")
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRraw[l][i] = new TH1F(Form("hSignalRraw_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahSignalZraw[l][i] = new TH1F(Form("hSignalZraw_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+              ahSignalR[l][i] = new TH1F(Form("hSignalR_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahSignalZ[l][i] = new TH1F(Form("hSignalZ_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+              ahSignalRnorm[l][i] = new TH1F(Form("hSignalRnorm_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahSignalZnorm[l][i] = new TH1F(Form("hSignalZnorm_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+            }
+          ahSignalRsub[i] = new TH1F(Form("hSignalRsub_pt_%d",i), ";#DeltaR;", nDrBins, drBins);
+          ahSignalZsub[i] = new TH1F(Form("hSignalZsub_pt_%d",i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+        }
+      return 0;
+    }
+  if(opt=="saveratio")
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRnormRatio[l][i] = new TH1F(Form("hSignalRnormRatio_%s_pt_%d",tRef[l].Data(),i), ";#DeltaR;", nDrBins, drBins);
+              ahSignalZnormRatio[l][i] = new TH1F(Form("hSignalZnormRatio_%s_pt_%d",tRef[l].Data(),i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+            }
+          ahSignalRsubRatio[i] = new TH1F(Form("hSignalRsubRatio_pt_%d",i), ";#DeltaR;", nDrBins, drBins);
+          ahSignalZsubRatio[i] = new TH1F(Form("hSignalZsubRatio_pt_%d",i), ";p_{T}^{D} / p_{T}^{jet};", nZBins, zBins);
+        }
+      return 0;
+    }
+  std::cout<<"error: invalid option for createhists()"<<std::endl;
+  return 1;
+}
+
+int writehists(Option_t* option)
+{
+  TString opt  = option;
+  opt.ToLower();
+
+  if(opt=="savetpl")
+    {
+      hNjets->Write(); // ... is it necessary
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahREfficiency[l][i]->Write();
+              ahNumREfficiency[l][i]->Write();
+              ahDenREfficiency[l][i]->Write();
+              ahZEfficiency[l][i]->Write();
+              ahNumZEfficiency[l][i]->Write();
+              ahDenZEfficiency[l][i]->Write();
+              for(int j=0;j<nDrBins;j++) 
+                {
+                  ahHistoRMassSignal[l][i][j]->Write();
+                  ahHistoRMassSwapped[l][i][j]->Write();
+                }
+              for(int j=0;j<nZBins;j++) 
+                {
+                  ahHistoZMassSignal[l][i][j]->Write();
+                  ahHistoZMassSwapped[l][i][j]->Write();
+                }
+            }
+        }
+      return 0;
+    }
+  if(opt=="savehist")
+    {
+      hNjets->Write();
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              for(int j=0;j<nDrBins;j++) ahHistoRMass[l][i][j]->Write();
+              for(int j=0;j<nZBins;j++) ahHistoZMass[l][i][j]->Write();
+            }
+        }
+      return 0;
+    }
+  if(opt.Contains("usehist"))
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRraw[l][i]->Write();
+              ahSignalZraw[l][i]->Write();
+              ahSignalR[l][i]->Write();
+              ahSignalZ[l][i]->Write();
+              ahSignalRnorm[l][i]->Write();
+              ahSignalZnorm[l][i]->Write();
+            }
+          ahSignalRsub[i]->Write();
+          ahSignalZsub[i]->Write();
+        }
+      return 0;
+    }
+  if(opt=="saveratio")
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRnormRatio[l][i]->Write();
+              ahSignalZnormRatio[l][i]->Write();
+            }
+          ahSignalRsubRatio[i]->Write();
+          ahSignalZsubRatio[i]->Write();
+        }
+      return 0;
+    }
+  std::cout<<"error: invalid option for writehists()"<<std::endl;
+  return 1;
+}
+
+int gethists(TFile* inf, Option_t* option)
+{
+  TString opt  = option;
+  opt.ToLower();
+  if(opt=="hist")
+    {
+      hNjets = (TH1F*)inf->Get("hNjets");
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              for(int j=0;j<nDrBins;j++) ahHistoRMass[l][i][j] = (TH1F*)inf->Get(Form("hHistoRMass_%s_pt_%d_dr_%d",tRef[l].Data(),i,j));
+              for(int j=0;j<nZBins;j++) ahHistoZMass[l][i][j] = (TH1F*)inf->Get(Form("hHistoZMass_%s_pt_%d_z_%d",tRef[l].Data(),i,j));
+            }
+        }
+      return 0;
+    }
+  if(opt=="tpl")
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahREfficiency[l][i] = (TH1F*)inf->Get(Form("hREfficiency_%s_pt_%d",tRef[l].Data(),i));
+              ahDenREfficiency[l][i] = (TH1F*)inf->Get(Form("hDenREfficiency_%s_pt_%d",tRef[l].Data(),i));
+              ahNumREfficiency[l][i] = (TH1F*)inf->Get(Form("hNumREfficiency_%s_pt_%d",tRef[l].Data(),i));
+              ahZEfficiency[l][i] = (TH1F*)inf->Get(Form("hZEfficiency_%s_pt_%d",tRef[l].Data(),i));
+              ahNumZEfficiency[l][i] = (TH1F*)inf->Get(Form("hNumZEfficiency_%s_pt_%d",tRef[l].Data(),i));
+              ahDenZEfficiency[l][i] = (TH1F*)inf->Get(Form("hDenZEfficiency_%s_pt_%d",tRef[l].Data(),i));
+              for(int j=0;j<nDrBins;j++) 
+                {
+                  ahHistoRMassSignal[l][i][j] = (TH1F*)inf->Get(Form("hHistoRMassSignal_%s_pt_%d_dr_%d",tRef[l].Data(),i,j));
+                  ahHistoRMassSwapped[l][i][j] = (TH1F*)inf->Get(Form("hHistoRMassSwapped_%s_pt_%d_dr_%d",tRef[l].Data(),i,j));
+                }
+              for(int j=0;j<nZBins;j++) 
+                {
+                  ahHistoZMassSignal[l][i][j] = (TH1F*)inf->Get(Form("hHistoZMassSignal_%s_pt_%d_z_%d",tRef[l].Data(),i,j));
+                  ahHistoZMassSwapped[l][i][j] = (TH1F*)inf->Get(Form("hHistoZMassSwapped_%s_pt_%d_z_%d",tRef[l].Data(),i,j));
+                }
+            }
+        }
+      return 0;
+    }
+  if(opt.Contains("plothist"))
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRnorm[l][i] = (TH1F*)inf->Get(Form("hSignalRnorm_%s_pt_%d",tRef[l].Data(),i));
+              ahSignalZnorm[l][i] = (TH1F*)inf->Get(Form("hSignalZnorm_%s_pt_%d",tRef[l].Data(),i));
+            }
+          ahSignalRsub[i] = (TH1F*)inf->Get(Form("hSignalRsub_pt_%d",i));
+          ahSignalZsub[i] = (TH1F*)inf->Get(Form("hSignalZsub_pt_%d",i));
+        }
+      return 0;
+    }
+  if(opt.Contains("saveratio") && (opt.Contains("pp")||opt.Contains("pbpb")))
+    {
+      Int_t isPbPb = opt.Contains("pp")?0:1;
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRnormP[isPbPb][l][i] = (TH1F*)inf->Get(Form("hSignalRnorm_%s_pt_%d",tRef[l].Data(),i));
+              ahSignalRnormP[isPbPb][l][i]->SetName(Form("hSignalRnorm_%s_pt_%d_%s",tRef[l].Data(),i,tPbPb[isPbPb].Data()));
+              ahSignalZnormP[isPbPb][l][i] = (TH1F*)inf->Get(Form("hSignalZnorm_%s_pt_%d",tRef[l].Data(),i));
+              ahSignalZnormP[isPbPb][l][i]->SetName(Form("hSignalZnorm_%s_pt_%d_%s",tRef[l].Data(),i,tPbPb[isPbPb].Data()));
+            }
+          ahSignalRsubP[isPbPb][i] = (TH1F*)inf->Get(Form("hSignalRsub_pt_%d",i));
+          ahSignalRsubP[isPbPb][i]->SetName(Form("hSignalRsub_pt_%d_%s",i,tPbPb[isPbPb].Data()));
+          ahSignalZsubP[isPbPb][i] = (TH1F*)inf->Get(Form("hSignalZsub_pt_%d",i));
+          ahSignalZsubP[isPbPb][i]->SetName(Form("hSignalZsub_pt_%d_%s",i,tPbPb[isPbPb].Data()));
+        }
+      return 0;
+    }
+  if(opt=="plotratio")
+    {
+      for(int i=0;i<nPtBins;i++)
+        {
+          for(int l=0;l<nRefBins;l++)
+            {
+              ahSignalRnormRatio[l][i] = (TH1F*)inf->Get(Form("hSignalRnormRatio_%s_pt_%d",tRef[l].Data(),i));
+              ahSignalZnormRatio[l][i] = (TH1F*)inf->Get(Form("hSignalZnormRatio_%s_pt_%d",tRef[l].Data(),i));
+            }
+          ahSignalRsubRatio[i] = (TH1F*)inf->Get(Form("hSignalRsubRatio_pt_%d",i));
+          ahSignalZsubRatio[i] = (TH1F*)inf->Get(Form("hSignalZsubRatio_pt_%d",i));
+        }
+      return 0;
+    }
+  std::cout<<"error: invalid option for gethists()"<<std::endl;
+  return 1;
+}
+
+
+
+#endif
---
+++ studymixedbackground/djtana_savehist.C
@@ -0,0 +1,254 @@
+#include "djtana.h"
+
+void djtana_savehist(TString inputname, TString outputname, 
+                     TString collisionsyst, Int_t isMC, Int_t irecogen,
+                     Float_t jetptmin, Float_t jetetamin, Float_t jetetamax, 
+                     TString hltsel="noHLT", Int_t jescale=0, Int_t gensmearpt=0, Int_t gensmearphi=0, Int_t maxevt=-1)
+{
+  int arguerr(TString collisionsyst, Int_t irecogen, Int_t isMC);
+  if(arguerr(collisionsyst, irecogen, isMC)) return;
+
+  if(createhists("savehist")) return;
+
+  Int_t ispp = collisionsyst=="pp"?1:0;
+  setnCentBins(ispp);
+ 
+  djet djt(inputname, ispp, isMC);
+  djt.setjetcut(jetptmin, jetetamin, jetetamax);
+  djt.setGcut(cutval_Dy);
+  initcutval(collisionsyst);
+
+  TRandom3* pRandom3 = new TRandom3();
+
+  std::vector<std::vector<Float_t>>* paramfScalePt = ispp?&paramfScalePt_pp:&paramfScalePt_PbPb;
+  std::vector<std::vector<Float_t>>* paramfResoPt = ispp?&paramfResoPtCorr_pp:&paramfResoPtCorr_PbPb;
+  std::vector<std::vector<Float_t>>* paramfScaleRecoPt = ispp?&paramfScaleRecoPt_pp:&paramfScaleRecoPt_PbPb;
+  std::vector<std::vector<Float_t>>* paramfScalePtFfCorr = ispp?&paramfScalePtFfCorr_pp:&paramfScalePtFfCorr_PbPb;
+  std::vector<std::vector<Float_t>>* paramRealfP0 = ispp?&paramRealfP0_pp:&paramRealfP0_PbPb;
+  std::vector<std::vector<Float_t>>* paramRealfP1 = ispp?&paramRealfP1_pp:&paramRealfP1_PbPb;
+
+  std::vector<std::vector<Float_t>>* paramfResoPhi = ispp?&paramfResoPhi_pp:&paramfResoPhi_PbPb;
+  std::vector<std::vector<Float_t>>* paramfResoEta = ispp?&paramfResoEta_pp:&paramfResoEta_PbPb;
+  std::vector<std::vector<Float_t>>* paramfResoDeltaPhi = ispp?&paramfResoDeltaPhi_pp:&paramfResoDeltaPhi_PbPb;
+  std::vector<std::vector<Float_t>>* paramfResoDeltaEta = ispp?&paramfResoDeltaEta_pp:&paramfResoDeltaEta_PbPb;
+
+  int64_t nentries = djt.fChain->GetEntriesFast();
+  int rnentries = (maxevt>0&&maxevt<=nentries)?maxevt:nentries;
+  int ncountjet = 0;
+  for(int i=0;i<rnentries;i++)
+    {
+      if(i%10000==0) std::cout<<std::setiosflags(std::ios::left)<<"  [ \033[1;36m"<<std::setw(10)<<i<<"\033[0m"<<" / "<<std::setw(10)<<rnentries<<" ] "<<"\033[1;36m"<<std::setw(4)<<Form("%.0f%s",100.*i/rnentries,"%")<<"\033[0m"<<"   >>   djtana_savehist("<<std::setw(5)<<Form("%s,",collisionsyst.Data())<<" "<<std::setw(5)<<Form("%s,",tMC[isMC].Data())<<" "<<std::setw(20)<<Form("%sD_%sjet)", djt.aDopt[irecogen].Data(), djt.ajetopt[irecogen].Data())<<"\r"<<std::flush;
+      //
+      djt.fChain->GetEntry(i);
+      djt.fHlt->GetEntry(i);
+      //
+
+      if(isMC && djt.pthat < 15) continue;
+
+      // to add event selection ...
+      if(djt.ishltselected(hltsel) < 0) return;
+      if(!djt.ishltselected(hltsel)) continue;
+
+      Int_t ibincent = ispp?0:xjjc::findibin(&centBins, (float)(djt.hiBin/2.));
+      if(ibincent<0) {std::cout<<"wrong ibincent"<<std::endl; return;}
+
+      // Float_t cweight = 1.;
+      Float_t cweight = ispp?1.:centweight[djt.hiBin];
+      // Float_t evtweight = 1;
+      Float_t evtweight = isMC?(djt.pthatweight*cweight):1.;
+
+      // loop jets
+      for(int jj=0;jj<*(djt.anjet[irecogen]);jj++)
+        {
+          if(isMC && (**djt.asubid[irecogen])[jj]!=0) continue;
+
+          /*********************************/
+
+          Float_t jetpt = (**djt.ajetpt[irecogen])[jj];
+          Float_t jetphi = (**djt.ajetphi[irecogen])[jj];
+          Float_t jeteta = (**djt.ajeteta[irecogen])[jj];
+
+          // JEC
+          if(djt.ajetopt[irecogen]=="reco" && jescale)
+            {
+              if(jescale==1)
+                {
+                  Float_t jetrecomatgenpt = jetpt / (paramfScaleRecoPt->at(ibincent).at(0) + paramfScaleRecoPt->at(ibincent).at(1)/TMath::Sqrt(jetpt) + paramfScaleRecoPt->at(ibincent).at(2)/jetpt + paramfScaleRecoPt->at(ibincent).at(3)/(jetpt*jetpt));
+                  Float_t vScalePt = paramfScalePt->at(ibincent).at(0) + paramfScalePt->at(ibincent).at(1)/TMath::Sqrt(jetrecomatgenpt) + paramfScalePt->at(ibincent).at(2)/jetrecomatgenpt + paramfScalePt->at(ibincent).at(3)/(jetrecomatgenpt*jetrecomatgenpt);
+                  jetpt = jetpt/vScalePt;
+                }
+              else if(jescale==2)
+                {
+                  Float_t vScaleFfPt = 1;
+                  Int_t ibinjtpt = xjjc::findibin(&jtptBins, jetpt);
+                  if(ibinjtpt >= 0)
+                    {
+                      vScaleFfPt = paramRealfP1->at(ibincent).at(ibinjtpt)*((*djt.jetnpfpart_akpu3pf)[jj]-paramRealfP0->at(ibincent).at(ibinjtpt));
+                    }
+                  jetpt = jetpt/vScaleFfPt;
+                  Float_t vScalePtFfCorr = paramfScalePtFfCorr->at(ibincent).at(0) + paramfScalePtFfCorr->at(ibincent).at(1)/TMath::Sqrt(jetpt) + paramfScalePtFfCorr->at(ibincent).at(2)/jetpt + paramfScalePtFfCorr->at(ibincent).at(3)/(jetpt*jetpt);
+                  jetpt = jetpt/vScalePtFfCorr;
+                }
+              else { std::cout<<"invalid jescale"<<std::endl; return; }
+            }
+          // smear pt
+          if(djt.ajetopt[irecogen]=="gen" && gensmearpt)
+            {
+              if(jetpt > 10)
+                {
+                  Float_t sigmaPt = TMath::Sqrt(paramfResoPt->at(ibincent).at(0)*paramfResoPt->at(ibincent).at(0) + 
+                                                paramfResoPt->at(ibincent).at(1)*paramfResoPt->at(ibincent).at(1)/jetpt + 
+                                                paramfResoPt->at(ibincent).at(2)*paramfResoPt->at(ibincent).at(2)/(jetpt*jetpt));
+                  jetpt = jetpt * pRandom3->Gaus(1, sigmaPt);
+                }
+            }
+
+          /*********************************/
+          if(jetpt < djt.cut_jetpt_min) continue;
+          /*********************************/
+
+          // smear angle
+          if(djt.ajetopt[irecogen]=="gen" && gensmearphi)
+            {
+              if(gensmearphi==1)
+                {
+                  Float_t sigmaPhi = TMath::Sqrt(paramfResoPhi->at(ibincent).at(0)*paramfResoPhi->at(ibincent).at(0) + 
+                                                 paramfResoPhi->at(ibincent).at(1)*paramfResoPhi->at(ibincent).at(1)/jetpt + 
+                                                 paramfResoPhi->at(ibincent).at(2)*paramfResoPhi->at(ibincent).at(2)/(jetpt*jetpt));
+                  jetphi = jetphi + pRandom3->Gaus(0, sigmaPhi);
+
+                  Float_t sigmaEta = TMath::Sqrt(paramfResoEta->at(ibincent).at(0)*paramfResoEta->at(ibincent).at(0) + 
+                                                 paramfResoEta->at(ibincent).at(1)*paramfResoEta->at(ibincent).at(1)/jetpt + 
+                                                 paramfResoEta->at(ibincent).at(2)*paramfResoEta->at(ibincent).at(2)/(jetpt*jetpt));
+                  jeteta = jeteta + pRandom3->Gaus(0, sigmaEta);
+                }
+              else if(gensmearphi==2)
+                {
+                  Float_t sigmaPhi = TMath::Sqrt(paramfResoDeltaPhi->at(ibincent).at(0)*paramfResoDeltaPhi->at(ibincent).at(0) +
+                                                 paramfResoDeltaPhi->at(ibincent).at(1)*paramfResoDeltaPhi->at(ibincent).at(1)/jetpt +
+                                                 paramfResoDeltaPhi->at(ibincent).at(2)*paramfResoDeltaPhi->at(ibincent).at(2)/(jetpt*jetpt)
+                                                 - (paramfResoDeltaPhi->at(ibincent).at(3)*paramfResoDeltaPhi->at(ibincent).at(3) +
+                                                    paramfResoDeltaPhi->at(ibincent).at(4)*paramfResoDeltaPhi->at(ibincent).at(4)/jetpt +
+                                                    paramfResoDeltaPhi->at(ibincent).at(5)*paramfResoDeltaPhi->at(ibincent).at(5)/(jetpt*jetpt)));
+                  jetphi = jetphi + pRandom3->Gaus(0, sigmaPhi);
+
+                  Float_t sigmaEta = TMath::Sqrt(paramfResoDeltaEta->at(ibincent).at(0)*paramfResoDeltaEta->at(ibincent).at(0) +
+                                                 paramfResoDeltaEta->at(ibincent).at(1)*paramfResoDeltaEta->at(ibincent).at(1)/jetpt +
+                                                 paramfResoDeltaEta->at(ibincent).at(2)*paramfResoDeltaEta->at(ibincent).at(2)/(jetpt*jetpt)
+                                                 - (paramfResoDeltaEta->at(ibincent).at(3)*paramfResoDeltaEta->at(ibincent).at(3) +
+                                                    paramfResoDeltaEta->at(ibincent).at(4)*paramfResoDeltaEta->at(ibincent).at(4)/jetpt +
+                                                    paramfResoDeltaEta->at(ibincent).at(5)*paramfResoDeltaEta->at(ibincent).at(5)/(jetpt*jetpt)));
+                  jeteta = jeteta + pRandom3->Gaus(0, sigmaEta);
+                }
+              else { std::cout<<"invalid gensmearphi"<<std::endl; return; }
+            }
+
+          /*********************************/
+          if(!(TMath::Abs(jeteta) > djt.cut_jeteta_min && TMath::Abs(jeteta) < djt.cut_jeteta_max)) continue;
+          /*********************************/
+
+          ncountjet += evtweight;
+
+          // loop D
+          for(int jd=0;jd<*(djt.anD[irecogen]);jd++)
+            {
+              Int_t ibinpt = xjjc::findibin(&ptBins, (**djt.aDpt[irecogen])[jd]);
+              if(ibinpt<0) continue;              
+
+              // to add pt-dependent event selection ...
+
+              Float_t deltaphi = TMath::ACos(TMath::Cos((**djt.aDphi[irecogen])[jd] - jetphi));
+              Float_t deltaeta = (**djt.aDeta[irecogen])[jd] - jeteta;
+              Float_t deltaetaref = (**djt.aDeta[irecogen])[jd] + jeteta;
+              Float_t deltaR[nRefBins] = {(float)TMath::Sqrt(pow(deltaphi, 2) + pow(deltaeta, 2)), 
+                                          (float)TMath::Sqrt(pow(deltaphi, 2) + pow(deltaetaref, 2))};
+              Float_t zvariable = (**djt.aDpt[irecogen])[jd]/jetpt;
+              
+              for(int l=0;l<nRefBins;l++)
+                {
+                  Int_t ibindr = xjjc::findibin(&drBins, deltaR[l]);
+                  if(ibindr<0) continue;
+                  Int_t result_initcutval = initcutval_bindep(collisionsyst, ibinpt, ibindr);
+                  if(result_initcutval) return;
+                  djt.settrkcut(cutval_trkPt, cutval_trkEta, cutval_trkPtErr);
+                  djt.setDcut(cutval_Dsvpv, cutval_Dalpha, cutval_Dchi2cl, cutval_Dy);
+                  Int_t djtDsel = djt.isDselected(jd, djt.aDopt[irecogen]);
+                  if(djtDsel < 0) {std::cout<<"error: invalid option for isDselected()"<<std::endl; return;}
+                  if(!djtDsel) continue;
+                  Float_t Dmass = irecogen%2==0?(*djt.Dmass)[jd]:DZERO_MASS;
+                  ahHistoRMass[l][ibinpt][ibindr]->Fill(Dmass, evtweight);
+
+                  if(deltaR[l]>0.3) continue; // ... to discuss
+                  Int_t ibinz = xjjc::findibin(&zBins, zvariable);
+                  if(ibinz<0) continue;
+                  ahHistoZMass[l][ibinpt][ibinz]->Fill(Dmass, evtweight);
+                }
+            }
+        }
+    }
+  std::cout<<std::setiosflags(std::ios::left)<<"  Processed "<<"\033[1;31m"<<rnentries<<"\033[0m out of\033[1;31m "<<nentries<<"\033[0m event(s)."<<"   >>   djtana_savehist("<<std::setw(5)<<Form("%s,",collisionsyst.Data())<<" "<<std::setw(5)<<Form("%s,",tMC[isMC].Data())<<" "<<std::setw(30)<<Form("%sD_%sjet)", djt.aDopt[irecogen].Data(), djt.ajetopt[irecogen].Data())<<std::endl;
+  std::cout<<std::endl;
+
+  hNjets->SetBinContent(1, ncountjet);
+
+  TFile* outf = new TFile(Form("%s.root",outputname.Data()), "recreate");
+  outf->cd();
+  if(writehists("savehist")) return;
+  outf->Write();
+  outf->Close();
+
+}
+
+int main(int argc, char* argv[])
+{
+  if(argc==14)
+    {
+      djtana_savehist(argv[1], argv[2], argv[3], atoi(argv[4]), atoi(argv[5]), atof(argv[6]), atof(argv[7]), atof(argv[8]), argv[9], atoi(argv[10]), atoi(argv[11]), atoi(argv[12]), atoi(argv[13]));
+      return 0;
+    }
+  if(argc==13)
+    {
+      djtana_savehist(argv[1], argv[2], argv[3], atoi(argv[4]), atoi(argv[5]), atof(argv[6]), atof(argv[7]), atof(argv[8]), argv[9], atoi(argv[10]), atoi(argv[11]), atoi(argv[12]));
+      return 0;
+    }
+  if(argc==10)
+    {
+      djtana_savehist(argv[1], argv[2], argv[3], atoi(argv[4]), atoi(argv[5]), atof(argv[6]), atof(argv[7]), atof(argv[8]), argv[9]);
+      return 0;
+    }
+  else if(argc==1)
+    {
+      djtana_savehist("/export/d00/scratch/jwang/Djets/data/DjetFiles_20171120_pp_5TeV_HighPtLowerJets_dPt4tkPt1p5Alpha0p2Decay2_D0Dstar_20170614.root",
+                      "test",
+                      "pp", 0, 0,
+                      40, 0.3, 1.6, 
+                      "HLTJet40Jet60", 1, 0, 0, 10000);
+      return 0;
+    }
+  else
+    {
+      std::cout<<"  Error: invalid arguments number - djtana_savehist()"<<std::endl;
+      return 1;
+    }
+}
+
+int arguerr(TString collisionsyst, Int_t irecogen, Int_t isMC)
+{
+  if(collsyst_list.find(collisionsyst)==collsyst_list.end())
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"collisionsyst\""<<std::endl;
+      return 1;
+    }
+  if(isMC!=0 && isMC!=1)
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"isMC\""<<std::endl;
+      return 1;
+    }
+  if(irecogen<0 && irecogen>3)
+    {
+      std::cout<<"\033[1;31merror:\033[0m invalid \"irecogen\""<<std::endl;
+      return 1;
+    }
+  return 0;
+}
+
